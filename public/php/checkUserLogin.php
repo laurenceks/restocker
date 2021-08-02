71691dc88efac893ad6e4ac099d7086917ab2e5a
laurenceks <lks@live.co.uk>
2021-08-02 10:07:59 +0100

    Initial check login api

--- public/php/checkUserLogin.php
+++ public/php/checkUserLogin.php
@@ -1,1 +1,9 @@
 <?php
+require 'vendor/autoload.php';
+use Delight\Auth\Auth;
+
+require_once "db.php";
+
+$auth = new Auth($db);
+
+echo json_encode($auth->isLoggedIn());
--- public/php/db.php
+++ public/php/db.php
@@ -1,1 +1,9 @@
 <?php
+require_once "dbCredentials.php";
+try {
+    $db = new PDO("mysql:host=$dbServer;dbname=$dbName", $dbUser, $dbPass);
+    // set the PDO error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
