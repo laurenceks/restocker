5a579b479e02f9d81031e849b12084ce5c065423
laurenceks <lks@live.co.uk>
2021-08-04 08:48:37 +0100

    Login now stores user data in global context

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,18 +1,33 @@
-import React, {useRef} from 'react';
-import PropTypes from 'prop-types';
+import {useContext, useRef, useState} from 'react';
 import LoginInput from "./loginComponents/LoginInput";
 import LoginCheckbox from "./loginComponents/LoginCheckbox";
-import {NavLink} from "react-router-dom";
 import LoginLink from "./loginComponents/LoginLink";
 import validateForm from "../../functions/formValidation.js"
+import LoginFeedback from "./LoginFeedback";
+import {GlobalAppContext} from "../../App";
+import {useHistory} from "react-router-dom";
 
-const login = form => {
-    //add onchange events to form validation context to get values
-    console.log("Login");
-}
 
 const Login = props => {
+    const [loginFeedback, setLoginFeedback] = useState(null);
     const loginForm = useRef();
+    const [globalAppContext, setGlobalAppContext] = useContext(GlobalAppContext);
+    const history = useHistory();
+    const login = formOutput => {
+        fetch("./php/login/login.php", {
+            method: "POST",
+            body: JSON.stringify(formOutput.values)
+        }).then((x) => {
+            x.json().then((x) => {
+                if (x.success) {
+                    setGlobalAppContext({...globalAppContext, isLoggedIn: true, loginCheckedOnce: true});
+                    history.push("/");
+                } else {
+                    setLoginFeedback({text: x.feedback, class: "bg-danger"})
+                }
+            })
+        });
+    }
 
     return (
         <form className={"loginForm align-middle"} ref={loginForm} onSubmit={(e) => {
@@ -30,6 +45,8 @@ const Login = props => {
             </div>
             <LoginCheckbox id={"inputLoginRemember"} label={"Remember me"}/>
             <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            {loginFeedback &&
+            <LoginFeedback feedbackText={loginFeedback.text} feedbackClass={loginFeedback.class}/>}
             <LoginLink to={"/forgotPassword"} label={"Forgot password"}/>
             <LoginLink to={"/register"} label={"Register"}/>
             <p className="my-3 text-muted">&copy; Laurence Summers 2021</p>
