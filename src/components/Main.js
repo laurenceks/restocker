1d10899aa6661a697f0c22028efe027f9af36d65
laurenceks <lks@live.co.uk>
2021-08-04 08:47:20 +0100

    Temporary logout link added for testing

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,24 @@
 import React, {useContext} from 'react';
+import {GlobalAppContext} from "../App";
 
 const Main = props => {
+    const [globalAppContext, setGlobalAppContext] = useContext(GlobalAppContext);
     return (
         <div>
             <h1>Restocker</h1>
             <p>You are logged in.</p>
             <p>The main app will go here</p>
+            <p><a onClick={(e) => {
+                fetch("./php/login/tempLogout.php", {
+                    method: "GET",
+                }).then((x) => {
+                    setGlobalAppContext({...globalAppContext, isLoggedIn: false});
+                });
+            }}>Logout</a></p>
         </div>
     );
 };
 
-Main.propTypes = {
-
-};
+Main.propTypes = {};
 
 export default Main;
