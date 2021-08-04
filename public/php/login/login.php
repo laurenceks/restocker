13d71dc1fe74881143afaf47a886f1e03caae6b3
laurenceks <lks@live.co.uk>
2021-08-04 08:54:03 +0100

    Added login file

---
+++ public/php/login/login.php
@@ -0,0 +1,28 @@
+<?php
+require '../vendor/autoload.php';
+
+use Delight\Auth\Auth;
+
+require_once "../common/db.php";
+
+$auth = new Auth($db);
+
+$input = json_decode(file_get_contents('php://input'), true);
+
+$output = array("success" => false, "feedback" => "An unknown error occurred");
+
+try {
+    $auth->login($input['inputLoginEmail'], $input['inputLoginPassword']);
+    $output["success"] = true;
+    $output["feedback"] = "User is logged in";
+} catch (\Delight\Auth\InvalidEmailException $e) {
+    $output["feedback"] = "Wrong email address";
+} catch (\Delight\Auth\InvalidPasswordException $e) {
+    $output["feedback"] = "Wrong password";
+} catch (\Delight\Auth\EmailNotVerifiedException $e) {
+    $output["feedback"] = "Email not verified";
+} catch (\Delight\Auth\TooManyRequestsException $e) {
+    $output["feedback"] = "Too many requests";
+}
+
+echo json_encode($output);
