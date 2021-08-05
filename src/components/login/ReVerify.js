53594c0e60b3b38fed3eef5a0ea3a94af94032b6
laurenceks <lks@live.co.uk>
2021-08-05 19:35:26 +0100

    Changed to used own form references, ids and submission handling

--- src/components/login/ReVerify.js
+++ src/components/login/ReVerify.js
@@ -1,59 +1,55 @@
-import {useContext, useRef, useState} from 'react';
+import {useRef, useState} from 'react';
 import LoginInput from "./loginComponents/LoginInput";
-import LoginCheckbox from "./loginComponents/LoginCheckbox";
 import LoginLink from "./loginComponents/LoginLink";
 import validateForm from "../../functions/formValidation.js"
 import LoginFeedback from "./LoginFeedback";
-import {GlobalAppContext} from "../../App";
 import {useHistory} from "react-router-dom";
 
 
-const Login = props => {
-    const [loginFeedback, setLoginFeedback] = useState(null);
-    const loginForm = useRef();
-    const [globalAppContext, setGlobalAppContext] = useContext(GlobalAppContext);
-    const history = useHistory();
-    const login = formOutput => {
-        fetch("./php/login/login.php", {
+const ReVerify = props => {
+    const [reVerifyFeedback, setReverifyFeedback] = useState(null);
+    const reVerifyForm = useRef();
+    const reVerify = formOutput => {
+        fetch("./php/login/resendVerify.php", {
             method: "POST",
             body: JSON.stringify(formOutput.values)
         }).then((x) => {
             x.json().then((x) => {
-                if (x.success) {
-                    setGlobalAppContext({...globalAppContext, isLoggedIn: true, loginCheckedOnce: true});
-                    history.push("/");
-                } else {
-                    setLoginFeedback({text: x.feedback, class: "bg-danger"})
-                }
+                setReverifyFeedback({
+                    ...reVerifyFeedback,
+                    success: x.success,
+                    text: x.feedback,
+                    class: x.success ? "bg-success" : "bg-danger",
+                })
             })
         });
     }
 
     return (
-        <form className={"loginForm align-middle"} ref={loginForm} onSubmit={(e) => {
-            validateForm(e, loginForm, login)
+        <form className={"loginForm align-middle"} ref={reVerifyForm} onSubmit={(e) => {
+            validateForm(e, reVerifyForm, reVerify)
         }} noValidate>
-            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-            <div className="loginFormInputGroup mb-3">
-                <LoginInput type={"email"} placeholder={"you@example.com"}
-                            label={"Email address"}
-                            id={"inputLoginEmail"}
-                            invalidFeedback={"Please enter your email address"}/>
-                <LoginInput type={"password"} placeholder={"Password"} label={"Password"}
-                            id={"inputLoginPassword"}
-                            invalidFeedback={"Please enter your password"}/>
-            </div>
-            <LoginCheckbox id={"inputLoginRemember"} label={"Remember me"}/>
-            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-            {loginFeedback &&
-            <LoginFeedback feedbackText={loginFeedback.text} feedbackClass={loginFeedback.class}/>}
-            <LoginLink to={"/forgotPassword"} label={"Forgot password"}/>
-            <LoginLink to={"/register"} label={"Register"}/>
+            <h1 className="h3 mb-3 fw-normal">Resend verification email</h1>
+            {!reVerifyFeedback && <>
+                <div className="reVerifyFormInputGroup mb-3">
+                    <LoginInput type={"email"} placeholder={"you@example.com"}
+                                label={"Email address"}
+                                id={"inputReVerifyEmail"}
+                                invalidFeedback={"Please enter your email address"}/>
+                </div>
+                <button className="w-100 btn btn-lg btn-primary" type="submit">Re-send verification email</button>
+            </>
+            }
+            {reVerifyFeedback &&
+            <LoginFeedback feedbackText={reVerifyFeedback.text} feedbackClass={reVerifyFeedback.class}/>}
+            {!reVerifyFeedback &&
+            <LoginLink to={"/forgotPassword"} label={"Forgot password"}/>}
+            <LoginLink to={"/login"} label={"Back to login"}/>
             <p className="my-3 text-muted">&copy; Laurence Summers 2021</p>
         </form>
     );
 };
 
-Login.propTypes = {};
+ReVerify.propTypes = {};
 
-export default Login;
+export default ReVerify;
