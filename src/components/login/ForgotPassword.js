a076da07d2c4966fe9e23a64e372c93bc86f4a21
laurenceks <lks@live.co.uk>
2021-08-09 09:14:35 +0100

    Forgot password email request now functioning

--- src/components/login/ForgotPassword.js
+++ src/components/login/ForgotPassword.js
@@ -1,33 +1,55 @@
-import React, {useRef} from 'react';
-import PropTypes from 'prop-types';
+import {useRef, useState} from 'react';
 import LoginInput from "./loginComponents/LoginInput";
-import LoginCheckbox from "./loginComponents/LoginCheckbox";
-import {NavLink} from "react-router-dom";
 import LoginLink from "./loginComponents/LoginLink";
 import validateForm from "../../functions/formValidation.js"
+import LoginFeedback from "./LoginFeedback";
+import {useHistory} from "react-router-dom";
 
-const forgot = (e) => {
-    console.log("Forgot!")
-}
-const ForgotPassword = props => {
-    const forgotPasswordForm = useRef();
+
+const Forgot = props => {
+    const [forgotFeedback, setForgotFeedback] = useState(null);
+    const forgotForm = useRef();
+    const forgot = formOutput => {
+        fetch("./php/login/forgot/forgot.php", {
+            method: "POST",
+            body: JSON.stringify(formOutput.values)
+        }).then((x) => {
+            x.text().then((x) => {
+                console.log(x);
+                x = JSON.parse(x);
+                setForgotFeedback({
+                    ...forgotFeedback,
+                    success: x.success,
+                    text: x.feedback,
+                    class: x.success ? "bg-success" : "bg-danger",
+                })
+            })
+        });
+    }
+
     return (
-        <form className={"loginForm align-middle"} ref={forgotPasswordForm} onSubmit={(e) => {
-            validateForm(e, forgotPasswordForm, forgot)
+        <form className={"loginForm align-middle"} ref={forgotForm} onSubmit={(e) => {
+            validateForm(e, forgotForm, forgot)
         }} noValidate>
             <h1 className="h3 mb-3 fw-normal">Forgot password</h1>
-            <div className="mb-3 loginFormInputGroup">
-                <LoginInput type={"email"} placeholder={"you@example.com"} label={"Email address"}
-                            id={"inputRegisterEmail"}
-                            invalidFeedback={"Please enter the email address of your account"}/>
-            </div>
-            <button className="w-100 btn btn-lg btn-primary" type="submit">Send password reset</button>
-            <LoginLink to={"/login"} label={"Login"}/>
+            {!forgotFeedback && <>
+                <div className="reVerifyFormInputGroup mb-3">
+                    <LoginInput type={"email"} placeholder={"you@example.com"}
+                                label={"Email address"}
+                                id={"inputForgotEmail"}
+                                invalidFeedback={"Please enter your email address"}/>
+                </div>
+                <button className="w-100 btn btn-lg btn-primary" type="submit">Send password reset</button>
+            </>
+            }
+            {forgotFeedback &&
+            <LoginFeedback feedbackText={forgotFeedback.text} feedbackClass={forgotFeedback.class}/>}
+            <LoginLink to={"/login"} label={"Back to login"}/>
             <p className="my-3 text-muted">&copy; Laurence Summers 2021</p>
         </form>
     );
 };
 
-ForgotPassword.propTypes = {};
+Forgot.propTypes = {};
 
-export default ForgotPassword;
+export default Forgot;
