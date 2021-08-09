2e92d346a079ab0220091da334237cc17b2317b7
laurenceks <lks@live.co.uk>
2021-08-09 09:11:12 +0100

    Added marginTop prop, reorganised classes

--- src/components/login/LoginFeedback.js
+++ src/components/login/LoginFeedback.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const LoginFeedback = ({feedbackClass, feedbackText}) => {
+const LoginFeedback = ({feedbackClass, feedbackText, marginTop}) => {
     return (
-        <div>
-            <p className={`p-3 mt-3 rounded text-light ${feedbackClass}`}>{feedbackText}</p>
+        <div className={`p-3 ${marginTop && "mt-3"} rounded ${feedbackClass}`}>
+            <p className="text-light m-0">{feedbackText}</p>
         </div>
     );
 };
 
 LoginFeedback.propTypes = {
     feedbackClass: PropTypes.string,
-    feedbackText: PropTypes.string
+    feedbackText: PropTypes.string,
+    marginTop: PropTypes.bool
 };
 
 LoginFeedback.defaultProps = {
     feedbackClass: "bg-danger",
-    feedbackText: "An unknown error occurred"
+    feedbackText: "An unknown error occurred",
+    marginTop: true
 }
 
 export default LoginFeedback;
