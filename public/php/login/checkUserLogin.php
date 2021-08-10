69a170f7ae07cd48590895dd097ff4908c2af095
laurenceks <lks@live.co.uk>
2021-08-10 20:19:03 +0100

    Added user info retrieval if checking login

--- public/php/login/checkUserLogin.php
+++ public/php/login/checkUserLogin.php
@@ -6,4 +6,11 @@ use Delight\Auth\Auth;
 require_once "../common/db.php";
 $auth = new Auth($db);
 
-echo json_encode($auth->isLoggedIn());
+$output = array("isLoggedIn" => $auth->isLoggedIn(), "user" => null);
+
+if($output["isLoggedIn"]){
+    require "../common/getUserInfo.php";
+    $output["user"] = getUserInfo($auth->getUserId());
+}
+
+echo json_encode($output);
