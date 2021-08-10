d89048e2220eb9b3a107333e353d8b2f21dfc9a5
laurenceks <lks@live.co.uk>
2021-08-10 11:22:37 +0100

    Registration now saves extra user fields

--- public/php/login/register.php
+++ public/php/login/register.php
@@ -14,7 +14,6 @@ $output = array("success" => false, "feedback" => "An unknown error occurred", "
 
 try {
     $userId = $auth->register($input['inputRegisterEmail'], $input['inputRegisterPassword'], null, function ($selector, $token) use ($input, &$output) {
-
         require_once "../common/sendSmtpMail.php";
         require_once "verify/verificationEmail.php";
 
@@ -22,6 +21,24 @@ try {
         $mailToSend = composeSmtpMail($input['inputRegisterEmail'], $input['inputRegisterFirstName'] . " " . $input['inputRegisterLastName'], "Verify your Restocker account", $emailParams["message"], $emailParams["messageAlt"]);
         $output["mail"] = sendSmtpMail($mailToSend);
     });
+    try {
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        //TODO check if new organisation and if so assign manager/admin role
+
+        // prepare sql and bind parameters
+        $addUserInfo = $db->prepare("INSERT INTO users_info (user_id, first_name, last_name, role, organisation) VALUES (:userId, :firstname, :lastname, null, :organisation)");
+        $addUserInfo->bindParam(':userId', $userId);
+        $addUserInfo->bindParam(':firstname', $input['inputRegisterFirstName']);
+        $addUserInfo->bindParam(':lastname', $input['inputRegisterLastName']);
+        //$addUserInfo->bindParam(':role', null);
+        $addUserInfo->bindParam(':organisation', $input['inputRegisterOrganisation']);
+
+        $addUserInfo->execute();
+
+    } catch (PDOException $e) {
+        echo $output["feedback"] = $e->getMessage();
+    }
 
     $output["success"] = true;
     $output["feedback"] = "You have successfully registered, please check " . $input["inputRegisterEmail"] . " for a verification link";
