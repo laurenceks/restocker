efcc0388ede8cd7dbf636fdd9963dd4a241e225a
laurenceks <lks@live.co.uk>
2021-08-13 12:08:20 +0100

    Added role retrieval to getUserInfo

--- public/php/common/getUserInfo.php
+++ public/php/common/getUserInfo.php
@@ -1,10 +1,18 @@
 <?php
-function getUserInfo($userId)
+function getUserInfo($userId, $auth = null)
 {
     require "db.php";
-    $getUserInfo = $db->prepare("SELECT * FROM users_info WHERE userId = :userId");
+    $getUserInfo = $db->prepare("
+        SELECT users.email, users_info.*, users_organisations.organisation
+        FROM users
+        LEFT JOIN users_info ON users.id = users_info.id
+        LEFT JOIN users_organisations ON users_info.organisationId = users_organisations.id
+        WHERE users.id = :userId
+        ");
     $getUserInfo->bindParam(':userId', $userId);
     $getUserInfo->execute();
 
-    return $getUserInfo->fetch(PDO::FETCH_OBJ);
+    $output = $getUserInfo->fetch(PDO::FETCH_OBJ);
+    $output->roles = $auth ? array_values($auth->getRoles()) : null;
+    return $output;
 }
