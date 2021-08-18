f9f1454771132c911ecd0ff27167e3b309e36d1d
laurenceks <lks@live.co.uk>
2021-08-18 19:44:44 +0100

    Added file to get all users for given organisation ID

---
+++ public/php/login/getAllUsers.php
@@ -0,0 +1,17 @@
+<?php
+require "userLoginSecurityCheck.php";
+require "../common/processResult.php";
+
+$input = json_decode(file_get_contents('php://input'), true);
+
+$getAllUsers = $db->prepare("
+        SELECT users.email, users.verified, users_info.*, users_organisations.*
+        FROM users
+        LEFT JOIN users_info ON users.id = users_info.id
+        LEFT JOIN users_organisations ON users_info.organisationId = users_organisations.id
+        WHERE users_info.organisationId = :organisationId");
+$getAllUsers->bindParam(':organisationId', $input["organisationId"]);
+$getAllUsers->execute();
+
+//echo json_encode(processResult($getAllUsers->fetchAll(PDO::FETCH_ASSOC), array("verified", "approved", "admin", "superAdmin")));
+echo json_encode($getAllUsers->fetchAll(PDO::FETCH_ASSOC));
