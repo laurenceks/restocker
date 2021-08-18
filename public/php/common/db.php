0457220bb53e4cb2b85226076a8c016fef7f857a
laurenceks <lks@live.co.uk>
2021-08-18 19:43:44 +0100

    Turned off strinify of PDO fetch results

--- public/php/common/db.php
+++ public/php/common/db.php
@@ -4,6 +4,9 @@ try {
     $db = new PDO("mysql:host=$dbServer;dbname=$dbName", $dbUser, $dbPass);
     // set the PDO error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //make sure retrieved numbers and null aren't converted to strings
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $db->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
