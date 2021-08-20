53d66eebcb4df06db3730d5dd93091d8c4e0c073
laurenceks <lks@live.co.uk>
2021-08-20 17:59:15 +0100

    Added check for non-sole super admin to getAllUsers

--- public/php/users/getAllUsers.php
+++ public/php/users/getAllUsers.php
@@ -1,10 +1,14 @@
 <?php
 require "../security/userLoginSecurityCheck.php";
 require "../security/userAdminRightsCheck.php";
-require "../common/processResult.php";
+require '../vendor/autoload.php';
+
+use Delight\Auth\Auth;
+
+$auth = new Auth($db);
 
 $input = json_decode(file_get_contents('php://input'), true);
-
+$output = array("users" => array(), "isOneOfManySuperAdmins" => false);
 $getAllUsers = $db->prepare("
         SELECT users.email, users.verified, users_info.*, users_organisations.*
         FROM users
@@ -13,6 +17,14 @@ $getAllUsers = $db->prepare("
         WHERE users_info.organisationId = :organisationId");
 $getAllUsers->bindParam(':organisationId', $input["organisationId"]);
 $getAllUsers->execute();
+$output["users"] = $getAllUsers->fetchAll(PDO::FETCH_ASSOC);
+
+if ($auth->hasRole(\Delight\Auth\Role::SUPER_ADMIN)) {
+    $isSuperAdminAndThereIsMoreThanOne = $db->prepare("SELECT COUNT(*) as result FROM `users_info` WHERE `superAdmin` = 1 AND `organisationId` = :organisationId");
+    $isSuperAdminAndThereIsMoreThanOne->bindParam(':organisationId', $input["organisationId"]);
+    $isSuperAdminAndThereIsMoreThanOne->execute();
+    $output["isOneOfManySuperAdmins"] = $isSuperAdminAndThereIsMoreThanOne->fetchColumn(0) > 1;
+}
 
 //echo json_encode(processResult($getAllUsers->fetchAll(PDO::FETCH_ASSOC), array("verified", "approved", "admin", "superAdmin")));
-echo json_encode($getAllUsers->fetchAll(PDO::FETCH_ASSOC));
+echo json_encode($output);
