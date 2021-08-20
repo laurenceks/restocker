ffd9996fbde6717199bb69d8e90815113c1862d0
laurenceks <lks@live.co.uk>
2021-08-20 18:05:21 +0100

    Changed to H3

--- src/components/Users/UsersTableSection.js
+++ src/components/Users/UsersTableSection.js
@@ -5,7 +5,7 @@ import UsersTable from "./UsersTable";
 const UsersTableSection = ({title, data}) => {
     return (
         <div className="my-3">
-            <h1>{title}</h1>
+            <h3>{title}</h3>
             <UsersTable title={"Active users"} headers={data.headers} rows={data.rows}/>
         </div>
     );
