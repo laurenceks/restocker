bf3b7feeaa77a2e589650a7b49f4deb0bf4c1154
laurenceks <lks@live.co.uk>
2021-10-11 16:44:31 +0100

    Switched to bindValue for $_SESSION values to avoid mutation of data types casued by bindParam

--- public/php/users/getAllUsers.php
+++ public/php/users/getAllUsers.php
@@ -14,13 +14,13 @@ $getAllUsers = $db->prepare("
         LEFT JOIN users_info ON users.id = users_info.userId
         LEFT JOIN users_organisations ON users_info.organisationId = users_organisations.id
         WHERE users_info.organisationId = :organisationId");
-$getAllUsers->bindParam(':organisationId', $_SESSION["user"]->organisationId);
+$getAllUsers->bindValue(':organisationId', $_SESSION["user"]->organisationId);
 $getAllUsers->execute();
 $output["users"] = $getAllUsers->fetchAll(PDO::FETCH_ASSOC);
 
 if ($auth->hasRole(\Delight\Auth\Role::SUPER_ADMIN)) {
     $isSuperAdminAndThereIsMoreThanOne = $db->prepare("SELECT COUNT(*) as result FROM `users_info` WHERE `superAdmin` = 1 AND `organisationId` = :organisationId");
-    $isSuperAdminAndThereIsMoreThanOne->bindParam(':organisationId', $_SESSION["user"]->organisationId);
+    $isSuperAdminAndThereIsMoreThanOne->bindValue(':organisationId', $_SESSION["user"]->organisationId);
     $isSuperAdminAndThereIsMoreThanOne->execute();
     $output["isOneOfManySuperAdmins"] = $isSuperAdminAndThereIsMoreThanOne->fetchColumn(0) > 1;
 }
