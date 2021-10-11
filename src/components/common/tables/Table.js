779309baae96365de02617a1c188f109b780e903
laurenceks <lks@live.co.uk>
2021-10-11 16:45:53 +0100

    Removed unecessary logging

--- src/components/common/tables/Table.js
+++ src/components/common/tables/Table.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import {IoCheckmarkCircleSharp, IoCloseCircleSharp} from "react-icons/all";
 
 const renderCellContent = (x) => {
-    console.log(x)
     if (!x.type) {
         return typeof x === "string" || typeof x === "number" ? x : "";
     } else {
