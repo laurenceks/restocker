d92b9f9f8ab612d2c17cc89a92d4cdc24aede238
laurenceks <lks@live.co.uk>
2021-10-13 17:44:21 +0100

    Added dummy transactions

--- src/components/common/dummyData.js
+++ src/components/common/dummyData.js
@@ -400,7 +400,7 @@ export const dummyItems = [{
     "currentStock": 15
 }]
 
-export const dummyLists = [
+export const dummyLists =
     [{
         "id": 1,
         "listName": "Violet",
@@ -1202,4 +1202,705 @@ export const dummyLists = [
             {}
         ]
     }]
-]
+
+export const dummyTransactions = [{
+    "id": 1,
+    "itemId": 3,
+    "type": "withdraw",
+    "change": 42,
+    "user": 2,
+    "timestamp": "2021-09-21 00:23:18"
+}, {
+    "id": 2,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": -50,
+    "user": 2,
+    "timestamp": "2021-09-18 12:05:10"
+}, {
+    "id": 3,
+    "itemId": 2,
+    "type": "withdraw",
+    "change": -17,
+    "user": 2,
+    "timestamp": "2021-10-06 03:59:34"
+}, {
+    "id": 4,
+    "itemId": 3,
+    "type": "restock",
+    "change": 22,
+    "user": 2,
+    "timestamp": "2021-09-23 18:42:50"
+}, {
+    "id": 5,
+    "itemId": 3,
+    "type": "withdraw",
+    "change": -47,
+    "user": 2,
+    "timestamp": "2021-09-21 03:51:51"
+}, {
+    "id": 6,
+    "itemId": 3,
+    "type": "restock",
+    "change": 2,
+    "user": 2,
+    "timestamp": "2021-09-19 03:18:48"
+}, {
+    "id": 7,
+    "itemId": 3,
+    "type": "withdraw",
+    "change": 22,
+    "user": 2,
+    "timestamp": "2021-09-05 11:56:13"
+}, {
+    "id": 8,
+    "itemId": 2,
+    "type": "restock",
+    "change": 9,
+    "user": 1,
+    "timestamp": "2021-09-15 13:51:38"
+}, {
+    "id": 9,
+    "itemId": 1,
+    "type": "restock",
+    "change": -38,
+    "user": 2,
+    "timestamp": "2021-09-20 08:48:56"
+}, {
+    "id": 10,
+    "itemId": 1,
+    "type": "withdraw",
+    "change": 12,
+    "user": 2,
+    "timestamp": "2021-10-10 13:52:28"
+}, {
+    "id": 11,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": 34,
+    "user": 2,
+    "timestamp": "2021-10-07 21:59:11"
+}, {
+    "id": 12,
+    "itemId": 1,
+    "type": "restock",
+    "change": 38,
+    "user": 1,
+    "timestamp": "2021-10-11 10:53:45"
+}, {
+    "id": 13,
+    "itemId": 1,
+    "type": "restock",
+    "change": -32,
+    "user": 2,
+    "timestamp": "2021-10-08 04:18:50"
+}, {
+    "id": 14,
+    "itemId": 1,
+    "type": "restock",
+    "change": 2,
+    "user": 2,
+    "timestamp": "2021-09-06 10:20:37"
+}, {
+    "id": 15,
+    "itemId": 1,
+    "type": "withdraw",
+    "change": -45,
+    "user": 2,
+    "timestamp": "2021-09-15 05:27:01"
+}, {
+    "id": 16,
+    "itemId": 1,
+    "type": "withdraw",
+    "change": -39,
+    "user": 2,
+    "timestamp": "2021-09-26 21:31:10"
+}, {
+    "id": 17,
+    "itemId": 2,
+    "type": "withdraw",
+    "change": -25,
+    "user": 2,
+    "timestamp": "2021-09-13 22:01:40"
+}, {
+    "id": 18,
+    "itemId": 1,
+    "type": "restock",
+    "change": -3,
+    "user": 2,
+    "timestamp": "2021-09-13 08:11:36"
+}, {
+    "id": 19,
+    "itemId": 1,
+    "type": "withdraw",
+    "change": 1,
+    "user": 2,
+    "timestamp": "2021-09-11 01:59:44"
+}, {
+    "id": 20,
+    "itemId": 4,
+    "type": "restock",
+    "change": -10,
+    "user": 2,
+    "timestamp": "2021-10-03 02:24:09"
+}, {
+    "id": 21,
+    "itemId": 2,
+    "type": "restock",
+    "change": 41,
+    "user": 2,
+    "timestamp": "2021-09-07 22:05:59"
+}, {
+    "id": 22,
+    "itemId": 1,
+    "type": "withdraw",
+    "change": 1,
+    "user": 1,
+    "timestamp": "2021-10-06 00:14:09"
+}, {
+    "id": 23,
+    "itemId": 1,
+    "type": "restock",
+    "change": -3,
+    "user": 1,
+    "timestamp": "2021-09-08 11:23:57"
+}, {
+    "id": 24,
+    "itemId": 3,
+    "type": "withdraw",
+    "change": 12,
+    "user": 1,
+    "timestamp": "2021-09-02 13:42:30"
+}, {
+    "id": 25,
+    "itemId": 4,
+    "type": "restock",
+    "change": 47,
+    "user": 2,
+    "timestamp": "2021-09-24 05:32:33"
+}, {
+    "id": 26,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": -47,
+    "user": 1,
+    "timestamp": "2021-09-06 14:47:05"
+}, {
+    "id": 27,
+    "itemId": 3,
+    "type": "restock",
+    "change": -42,
+    "user": 2,
+    "timestamp": "2021-09-22 15:42:33"
+}, {
+    "id": 28,
+    "itemId": 3,
+    "type": "withdraw",
+    "change": -24,
+    "user": 2,
+    "timestamp": "2021-09-16 03:52:16"
+}, {
+    "id": 29,
+    "itemId": 2,
+    "type": "withdraw",
+    "change": 3,
+    "user": 2,
+    "timestamp": "2021-09-20 08:36:54"
+}, {
+    "id": 30,
+    "itemId": 1,
+    "type": "restock",
+    "change": 0,
+    "user": 2,
+    "timestamp": "2021-09-17 16:29:40"
+}, {
+    "id": 31,
+    "itemId": 4,
+    "type": "restock",
+    "change": 31,
+    "user": 2,
+    "timestamp": "2021-09-07 19:32:04"
+}, {
+    "id": 32,
+    "itemId": 4,
+    "type": "restock",
+    "change": -37,
+    "user": 2,
+    "timestamp": "2021-09-15 21:00:09"
+}, {
+    "id": 33,
+    "itemId": 1,
+    "type": "withdraw",
+    "change": 3,
+    "user": 2,
+    "timestamp": "2021-09-05 01:16:35"
+}, {
+    "id": 34,
+    "itemId": 1,
+    "type": "withdraw",
+    "change": 17,
+    "user": 2,
+    "timestamp": "2021-09-09 23:40:16"
+}, {
+    "id": 35,
+    "itemId": 2,
+    "type": "withdraw",
+    "change": -2,
+    "user": 2,
+    "timestamp": "2021-10-08 04:41:40"
+}, {
+    "id": 36,
+    "itemId": 3,
+    "type": "restock",
+    "change": -46,
+    "user": 2,
+    "timestamp": "2021-09-11 03:22:33"
+}, {
+    "id": 37,
+    "itemId": 2,
+    "type": "withdraw",
+    "change": 46,
+    "user": 1,
+    "timestamp": "2021-10-09 22:16:26"
+}, {
+    "id": 38,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": 49,
+    "user": 2,
+    "timestamp": "2021-10-02 06:26:48"
+}, {
+    "id": 39,
+    "itemId": 4,
+    "type": "restock",
+    "change": -20,
+    "user": 1,
+    "timestamp": "2021-10-10 06:45:15"
+}, {
+    "id": 40,
+    "itemId": 2,
+    "type": "restock",
+    "change": 31,
+    "user": 1,
+    "timestamp": "2021-10-04 12:20:31"
+}, {
+    "id": 41,
+    "itemId": 1,
+    "type": "withdraw",
+    "change": -9,
+    "user": 2,
+    "timestamp": "2021-09-16 13:22:12"
+}, {
+    "id": 42,
+    "itemId": 2,
+    "type": "restock",
+    "change": -19,
+    "user": 2,
+    "timestamp": "2021-09-24 23:27:08"
+}, {
+    "id": 43,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": 49,
+    "user": 2,
+    "timestamp": "2021-09-29 10:41:15"
+}, {
+    "id": 44,
+    "itemId": 2,
+    "type": "withdraw",
+    "change": 39,
+    "user": 2,
+    "timestamp": "2021-09-30 08:45:11"
+}, {
+    "id": 45,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": -2,
+    "user": 2,
+    "timestamp": "2021-10-03 02:05:02"
+}, {
+    "id": 46,
+    "itemId": 1,
+    "type": "withdraw",
+    "change": -47,
+    "user": 1,
+    "timestamp": "2021-10-09 21:40:25"
+}, {
+    "id": 47,
+    "itemId": 1,
+    "type": "withdraw",
+    "change": -8,
+    "user": 2,
+    "timestamp": "2021-09-01 11:18:52"
+}, {
+    "id": 48,
+    "itemId": 3,
+    "type": "withdraw",
+    "change": -38,
+    "user": 2,
+    "timestamp": "2021-09-20 19:31:13"
+}, {
+    "id": 49,
+    "itemId": 3,
+    "type": "restock",
+    "change": -23,
+    "user": 1,
+    "timestamp": "2021-09-18 21:16:51"
+}, {
+    "id": 50,
+    "itemId": 1,
+    "type": "withdraw",
+    "change": -30,
+    "user": 2,
+    "timestamp": "2021-09-14 22:54:36"
+}, {
+    "id": 51,
+    "itemId": 2,
+    "type": "withdraw",
+    "change": 32,
+    "user": 2,
+    "timestamp": "2021-10-03 18:58:35"
+}, {
+    "id": 52,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": -17,
+    "user": 1,
+    "timestamp": "2021-09-26 10:00:25"
+}, {
+    "id": 53,
+    "itemId": 3,
+    "type": "restock",
+    "change": 19,
+    "user": 1,
+    "timestamp": "2021-09-29 01:30:30"
+}, {
+    "id": 54,
+    "itemId": 2,
+    "type": "withdraw",
+    "change": -33,
+    "user": 2,
+    "timestamp": "2021-09-07 13:53:07"
+}, {
+    "id": 55,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": -26,
+    "user": 2,
+    "timestamp": "2021-10-11 08:40:37"
+}, {
+    "id": 56,
+    "itemId": 1,
+    "type": "withdraw",
+    "change": -33,
+    "user": 1,
+    "timestamp": "2021-10-10 10:06:09"
+}, {
+    "id": 57,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": -20,
+    "user": 2,
+    "timestamp": "2021-09-18 11:58:58"
+}, {
+    "id": 58,
+    "itemId": 1,
+    "type": "restock",
+    "change": -33,
+    "user": 1,
+    "timestamp": "2021-10-10 22:16:40"
+}, {
+    "id": 59,
+    "itemId": 3,
+    "type": "withdraw",
+    "change": -39,
+    "user": 2,
+    "timestamp": "2021-10-04 10:49:00"
+}, {
+    "id": 60,
+    "itemId": 3,
+    "type": "withdraw",
+    "change": -24,
+    "user": 2,
+    "timestamp": "2021-09-09 11:39:26"
+}, {
+    "id": 61,
+    "itemId": 3,
+    "type": "withdraw",
+    "change": -8,
+    "user": 1,
+    "timestamp": "2021-09-16 12:09:27"
+}, {
+    "id": 62,
+    "itemId": 1,
+    "type": "restock",
+    "change": 45,
+    "user": 2,
+    "timestamp": "2021-10-09 04:07:48"
+}, {
+    "id": 63,
+    "itemId": 2,
+    "type": "withdraw",
+    "change": -38,
+    "user": 1,
+    "timestamp": "2021-10-12 15:41:56"
+}, {
+    "id": 64,
+    "itemId": 2,
+    "type": "withdraw",
+    "change": -48,
+    "user": 2,
+    "timestamp": "2021-09-15 09:17:46"
+}, {
+    "id": 65,
+    "itemId": 2,
+    "type": "withdraw",
+    "change": -18,
+    "user": 2,
+    "timestamp": "2021-09-15 00:29:28"
+}, {
+    "id": 66,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": -46,
+    "user": 1,
+    "timestamp": "2021-09-24 05:00:10"
+}, {
+    "id": 67,
+    "itemId": 4,
+    "type": "restock",
+    "change": 41,
+    "user": 2,
+    "timestamp": "2021-10-01 23:09:47"
+}, {
+    "id": 68,
+    "itemId": 2,
+    "type": "restock",
+    "change": 47,
+    "user": 2,
+    "timestamp": "2021-09-03 09:58:47"
+}, {
+    "id": 69,
+    "itemId": 1,
+    "type": "restock",
+    "change": -31,
+    "user": 2,
+    "timestamp": "2021-09-27 12:08:04"
+}, {
+    "id": 70,
+    "itemId": 1,
+    "type": "restock",
+    "change": 23,
+    "user": 2,
+    "timestamp": "2021-09-17 23:27:57"
+}, {
+    "id": 71,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": -46,
+    "user": 1,
+    "timestamp": "2021-09-29 11:06:32"
+}, {
+    "id": 72,
+    "itemId": 2,
+    "type": "restock",
+    "change": -28,
+    "user": 2,
+    "timestamp": "2021-10-09 07:20:29"
+}, {
+    "id": 73,
+    "itemId": 3,
+    "type": "restock",
+    "change": -13,
+    "user": 1,
+    "timestamp": "2021-10-04 00:43:18"
+}, {
+    "id": 74,
+    "itemId": 3,
+    "type": "restock",
+    "change": 43,
+    "user": 1,
+    "timestamp": "2021-09-02 13:58:46"
+}, {
+    "id": 75,
+    "itemId": 1,
+    "type": "restock",
+    "change": 19,
+    "user": 2,
+    "timestamp": "2021-09-22 08:47:30"
+}, {
+    "id": 76,
+    "itemId": 1,
+    "type": "restock",
+    "change": 44,
+    "user": 2,
+    "timestamp": "2021-09-01 19:25:49"
+}, {
+    "id": 77,
+    "itemId": 1,
+    "type": "withdraw",
+    "change": -50,
+    "user": 2,
+    "timestamp": "2021-09-11 21:34:57"
+}, {
+    "id": 78,
+    "itemId": 1,
+    "type": "restock",
+    "change": -1,
+    "user": 2,
+    "timestamp": "2021-09-14 01:32:34"
+}, {
+    "id": 79,
+    "itemId": 2,
+    "type": "restock",
+    "change": 28,
+    "user": 1,
+    "timestamp": "2021-10-12 15:49:08"
+}, {
+    "id": 80,
+    "itemId": 1,
+    "type": "withdraw",
+    "change": 5,
+    "user": 1,
+    "timestamp": "2021-09-25 12:13:55"
+}, {
+    "id": 81,
+    "itemId": 2,
+    "type": "restock",
+    "change": 21,
+    "user": 1,
+    "timestamp": "2021-09-18 08:58:30"
+}, {
+    "id": 82,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": 46,
+    "user": 2,
+    "timestamp": "2021-10-11 11:14:55"
+}, {
+    "id": 83,
+    "itemId": 2,
+    "type": "withdraw",
+    "change": 36,
+    "user": 2,
+    "timestamp": "2021-09-21 01:14:48"
+}, {
+    "id": 84,
+    "itemId": 4,
+    "type": "restock",
+    "change": -46,
+    "user": 2,
+    "timestamp": "2021-09-15 16:45:20"
+}, {
+    "id": 85,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": -45,
+    "user": 1,
+    "timestamp": "2021-09-09 21:21:20"
+}, {
+    "id": 86,
+    "itemId": 1,
+    "type": "withdraw",
+    "change": 48,
+    "user": 1,
+    "timestamp": "2021-09-10 21:12:04"
+}, {
+    "id": 87,
+    "itemId": 1,
+    "type": "restock",
+    "change": -9,
+    "user": 1,
+    "timestamp": "2021-09-03 12:03:50"
+}, {
+    "id": 88,
+    "itemId": 2,
+    "type": "restock",
+    "change": 20,
+    "user": 2,
+    "timestamp": "2021-09-28 10:31:20"
+}, {
+    "id": 89,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": -17,
+    "user": 1,
+    "timestamp": "2021-09-24 21:08:40"
+}, {
+    "id": 90,
+    "itemId": 4,
+    "type": "restock",
+    "change": -46,
+    "user": 1,
+    "timestamp": "2021-09-26 09:24:49"
+}, {
+    "id": 91,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": -32,
+    "user": 2,
+    "timestamp": "2021-10-11 17:02:15"
+}, {
+    "id": 92,
+    "itemId": 4,
+    "type": "withdraw",
+    "change": 45,
+    "user": 1,
+    "timestamp": "2021-09-03 05:15:53"
+}, {
+    "id": 93,
+    "itemId": 4,
+    "type": "restock",
+    "change": -17,
+    "user": 2,
+    "timestamp": "2021-09-05 00:14:20"
+}, {
+    "id": 94,
+    "itemId": 2,
+    "type": "restock",
+    "change": -44,
+    "user": 2,
+    "timestamp": "2021-10-03 20:14:39"
+}, {
+    "id": 95,
+    "itemId": 2,
+    "type": "restock",
+    "change": 43,
+    "user": 2,
+    "timestamp": "2021-09-13 16:56:56"
+}, {
+    "id": 96,
+    "itemId": 2,
+    "type": "restock",
+    "change": -48,
+    "user": 1,
+    "timestamp": "2021-09-23 09:36:54"
+}, {
+    "id": 97,
+    "itemId": 3,
+    "type": "restock",
+    "change": -6,
+    "user": 1,
+    "timestamp": "2021-09-07 17:04:42"
+}, {
+    "id": 98,
+    "itemId": 2,
+    "type": "withdraw",
+    "change": 23,
+    "user": 2,
+    "timestamp": "2021-09-16 15:11:31"
+}, {
+    "id": 99,
+    "itemId": 2,
+    "type": "restock",
+    "change": -7,
+    "user": 1,
+    "timestamp": "2021-09-17 22:20:47"
+}, {
+    "id": 100,
+    "itemId": 2,
+    "type": "withdraw",
+    "change": -33,
+    "user": 1,
+    "timestamp": "2021-09-14 05:35:00"
+}]
