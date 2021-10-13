99c742a5e98ddaaeed6db955bb97167c9801b6d1
laurenceks <lks@live.co.uk>
2021-10-13 17:44:58 +0100

    Added self-contained state and defaultChecked

--- src/components/common/forms/InputCheckbox.js
+++ src/components/common/forms/InputCheckbox.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import {useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 import InputFeedbackTooltip from "./InputFeedbackTooltip";
 
-const InputCheckbox = ({id, label, inputClass, invalidFeedback, checkRequired, type, onChange, name, className}) => {
+const InputCheckbox = ({
+                           id,
+                           label,
+                           inputClass,
+                           invalidFeedback,
+                           checkRequired,
+                           type,
+                           onChange,
+                           name,
+                           className,
+                           defaultChecked
+                       }) => {
+    const [inputState, setInputState] = useState(defaultChecked);
+    const inputRef = useRef();
     return (
-        <div className={`form-check checkbox ${className && `${className}`}`}>
-            <input type={type} id={id} className={`form-check-input ${inputClass && ` ${inputClass}`}`}
+        <div className={`form-check checkbox cursor-pointer ${className && `${className}`}`} onClick={() => {
+            inputRef.current.click()
+        }}>
+            <input type={type}
+                   id={id}
+                   className={`form-check-input ${inputClass && ` ${inputClass}`}`}
                    data-checkrequired={checkRequired}
                    name={name}
+                   ref={inputRef}
                    onChange={(e) => {
+                       setInputState(e.target.value);
                        if (onChange) {
                            onChange(id, e.target.value)
                        }
-                   }}/>
+                   }}
+                   defaultChecked={defaultChecked}
+            />
             <label htmlFor={id} className={"form-check-label"}>
                 {label}
             </label>
@@ -22,26 +43,28 @@ const InputCheckbox = ({id, label, inputClass, invalidFeedback, checkRequired, t
 };
 
 InputCheckbox.propTypes = {
-    placeholder: PropTypes.bool,
     checkRequired: PropTypes.bool,
-    id: PropTypes.string,
-    label: PropTypes.string,
-    inputClass: PropTypes.string,
+    defaultChecked: PropTypes.bool,
+    placeholder: PropTypes.bool,
     className: PropTypes.string,
+    id: PropTypes.string,
+    inputClass: PropTypes.string,
     invalidFeedback: PropTypes.string,
-    type: PropTypes.string,
+    label: PropTypes.string,
     name: PropTypes.string,
+    type: PropTypes.string,
     onChange: PropTypes.func
 };
 InputCheckbox.defaultProps = {
-    placeholder: false,
     checkRequired: false,
+    defaultChecked: false,
+    placeholder: false,
     id: "inputCheckbox-" + Math.random(),
     label: "Checkbox",
+    type: "checkbox",
     inputClass: null,
     className: null,
     invalidFeedback: null,
-    type: "checkbox",
     name: null,
     onChange: null
 };
