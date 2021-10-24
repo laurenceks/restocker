a747b12e5a5feca15ac2d570e4df5c288d0d8308
laurenceks <lks@live.co.uk>
2021-10-24 10:45:12 +0100

    Removed unecessary import

--- src/components/common/styles.js
+++ src/components/common/styles.js
@@ -1,5 +1,3 @@
-import {Chart} from "chart.js";
-
 const computedStyle = getComputedStyle(document.documentElement, null);
 const bootstrapColourNames = ["--bs-blue", "--bs-indigo", "--bs-purple", "--bs-pink", "--bs-red", "--bs-orange", "--bs-yellow", "--bs-green", "--bs-teal", "--bs-cyan", "--bs-white", "--bs-gray", "--bs-gray-dark", "--bs-primary", "--bs-secondary", "--bs-success", "--bs-info", "--bs-warning", "--bs-danger", "--bs-light", "--bs-dark", "--bs-font-sans-serif", "--bs-font-monospace", "--bs-gradient"]
 const bootstrapVariables = {};
