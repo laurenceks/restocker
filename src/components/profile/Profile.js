e7ece47c5956f848c00a3f357f9f8d0d82e5ac97
Laurence Summers <lks@live.co.uk>
2021-11-04 17:36:26 +0000

    Fixed error where global app context not being retrieved correctly

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,34 +1,35 @@
-import PropTypes from 'prop-types';
 import FormInput from "../common/forms/FormInput";
-import {useEffect, useRef, useState} from "react"
-import InputCheckbox from "../common/forms/InputCheckbox";
-import fetchAllItems from "../../functions/fetchAllItems";
+import {useContext, useEffect, useRef, useState} from "react"
 import validateForm from "../../functions/formValidation";
 import fetchJson from "../../functions/fetchJson";
 import {GlobalAppContext} from "../../App";
 
 const Profile = () => {
 
+    const globalAppContext = useContext(GlobalAppContext);
+
+    console.log(globalAppContext);
+
     const [user, setUser] = useState({
-        email: GlobalAppContext.user.email,
-        firstName: GlobalAppContext.user.firstName,
-        lastName: GlobalAppContext.user.lastName
+        email: globalAppContext[0].user.email,
+        firstName: globalAppContext[0].user.firstName,
+        lastName: globalAppContext[0].user.lastName
     });
 
     const profileFormRef = useRef();
 
-
     const editProfile = (e) => {
-        fetchJson("./php/items/addTransaction.php", {
+        console.log(user)
+        fetchJson("./php/profile/editProfile.php", {
             method: "POST",
-            body: JSON.stringify(),
+            body: JSON.stringify(user),
         }, (x) => {
         });
     }
 
     return (
         <div className="container">
-            <div className={"row align-items-center"}>
+            <div className={"row align-items-center mb-3"}>
                 <h1>User profile</h1>
             </div>
             <form ref={profileFormRef}
@@ -37,55 +38,49 @@ const Profile = () => {
                       validateForm(e, profileFormRef, editProfile);
                   }}>
                 <div className="row align-items-center">
-                    <div className={"col"}>
-                        <div className={"row align-items-center"}>
-                            <div className="col-12 col-md-2 mb-3">
-                                <p className="m-0">ID {GlobalAppContext.user.id}</p>
-                            </div>
-                            <div className="col-12 col-md-4 mb-3">
-                                <FormInput type={"text"}
-                                           id={"inputProfileFirstName"}
-                                           label={"First name"}
-                                           onChange={(id, val) => {
-                                               setUser({
-                                                   ...user,
-                                                   firstName: val
-                                               });
-                                           }}
-                                           value={user.firstName}
-                                />
-                            </div>
-                            <div className="col-12 col-md-4 mb-3">
-                                <FormInput type={"text"}
-                                           id={"inputProfileLastName"}
-                                           label={"Last name"}
-                                           onChange={(id, val) => {
-                                               setUser({
-                                                   ...user,
-                                                   lastName: val
-                                               });
-                                           }}
-                                           value={user.lastName}
-                                />
-                            </div>
-                            <div className="col-12 col-md-4 mb-3">
-                                <FormInput type={"text"}
-                                           id={"inputProfileEmail"}
-                                           label={"Email address"}
-                                           onChange={(id, val) => {
-                                               setUser({
-                                                   ...user,
-                                                   email: val
-                                               });
-                                           }}
-                                           value={user.firstName}
-                                />
-                            </div>
-                        </div>
+                    <div className="col-12 col-md-1 mb-3 mb-md-0">
+                        <p className="m-0">ID {globalAppContext[0].user.id}</p>
                     </div>
-                </div>
-                <div className={"row"}>
-                    <div className="col-12 col-sm-2">
+                    <div className="col-12 col-md-3 mb-3 mb-md-0">
+                        <FormInput type={"text"}
+                                   id={"inputProfileFirstName"}
+                                   label={"First name"}
+                                   onChange={(id, val) => {
+                                       setUser({
+                                           ...user,
+                                           firstName: val
+                                       });
+                                   }}
+                                   value={user.firstName}
+                        />
+                    </div>
+                    <div className="col-12 col-md-3 mb-3 mb-md-0">
+                        <FormInput type={"text"}
+                                   id={"inputProfileLastName"}
+                                   label={"Last name"}
+                                   onChange={(id, val) => {
+                                       setUser({
+                                           ...user,
+                                           lastName: val
+                                       });
+                                   }}
+                                   value={user.lastName}
+                        />
+                    </div>
+                    <div className="col-12 col-md-3 mb-3 mb-md-0">
+                        <FormInput type={"text"}
+                                   id={"inputProfileEmail"}
+                                   label={"Email address"}
+                                   onChange={(id, val) => {
+                                       setUser({
+                                           ...user,
+                                           email: val
+                                       });
+                                   }}
+                                   value={user.email}
+                        />
+                    </div>
+                    <div className="col-12 col-md-2">
                         <button type="submit" className="btn btn-primary w-100">Update</button>
                     </div>
                 </div>
