3ade5a9698ec3039802044412936def91649f0a4
Laurence Summers <lks@live.co.uk>
2021-11-04 19:41:13 +0000

    Improved layout and swtiched to bootstrap grouped radio buttons

--- src/components/transactions/TransactionForm.js
+++ src/components/transactions/TransactionForm.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import FormInput from "../common/forms/FormInput";
 import {useEffect, useRef, useState} from "react"
 
-import {mockLists} from "../common/mockData";
+import {mockLists, mockLocations} from "../common/mockData";
 import InputCheckbox from "../common/forms/InputCheckbox";
 import fetchAllItems from "../../functions/fetchAllItems";
 import validateForm from "../../functions/formValidation";
@@ -20,6 +20,7 @@ const TransactionForm = ({formType}) => {
             this.quantity = 0;
             this.displayQuantity = "";
             this.unit = "";
+            this.locationid = null;
         }
     }
 
@@ -81,103 +82,111 @@ const TransactionForm = ({formType}) => {
 
     return (
         <div className="container">
-            <div className={"row align-items-center"}>
-                <h1>{formType === "withdraw" ? `Withdraw ${withdrawItemType}` : "Restock item"}</h1>
-            </div>
             <form ref={transactionFormRef}
                   id={"withdrawForm"}
                   onSubmit={(e) => {
                       validateForm(e, transactionFormRef, withdrawItemType === "item" ? withdrawItems : withdrawList);
                   }}>
-                <div className="row align-items-center">
-                    {formType === "withdraw" && <div className={"col-12 col-md-5 mb-3"}>
-                        <div className={"row align-items-center"}>
-                            <h5>Type</h5>
+                <div className={"row align-items-center"}>
+                    <h1>{formType === "withdraw" ? `Withdraw ${withdrawItemType}` : "Restock item"}</h1>
+                </div>
+                {formType === "withdraw" &&
+                <div className="row align-items-center mb-3">
+                    <div className="col-12 col-md-1 mb-3 mb-md-0">
+                        <h5 className="d-inline-block ">Type</h5>
+                    </div>
+                    <div className="col-12 col-md-11">
+                        <div className="btn-group" role="group" aria-label="Withdraw list type">
+                            <input type="radio" className="btn-check" name="btnradio" id="btnradio1"
+                                   autoComplete="off" checked/>
+                            <label className="btn btn-outline-primary" htmlFor="btnradio1">List</label>
+                            <input type="radio" className="btn-check" name="btnradio" id="btnradio2"
+                                   autoComplete="off"/>
+                            <label className="btn btn-outline-primary" htmlFor="btnradio2">item</label>
                         </div>
-                        <div className={"row align-items-center bg-light rounded p-2"}>
-                            <div
-                                className={`col-6 ${withdrawItemType === "item" && "bg-primary rounded text-light"}`}>
-                                <InputCheckbox className="my-3" type="radio"
-                                               name="inputWithdrawType"
-                                               id="inputWithdrawTypeItem"
-                                               label="Item"
-                                               onChange={(id, e) => {
-                                                   withdrawItemType("item")
-                                                   setTransactionData(new transactionDataTemplate("item"));
-                                               }}
-                                               defaultChecked={withdrawItemType === "item"}
-                                />
-                            </div>
-                            <div
-                                className={`col-6 ${withdrawItemType === "list" && "bg-primary rounded text-light"}`}>
-                                <InputCheckbox className="my-3" type="radio"
-                                               name="inputWithdrawType"
-                                               id="inputWithdrawTypeList"
-                                               label="List"
-                                               onChange={(id, e) => {
-                                                   withdrawItemType("list")
-                                                   setTransactionData(new transactionDataTemplate("list"));
-                                               }}/>
-                            </div>
+                    </div>
+                </div>
+                }
+                <div className={"row align-items-center"}>
+                    <h5>{withdrawItemType === "item" ? "Item" : "List"}</h5>
+                </div>
+                <div className={"row align-items-center"}>
+                    <div className="col-12 col-md-1 mb-3">
+                        <p className="m-0">ID {transactionData.id}</p>
+                    </div>
+                    <div className="col-12 col-md-4 mb-3">
+                        <FormInput
+                            type={"typeahead"}
+                            id={"inputWithdrawName"}
+                            typeaheadProps={{
+                                inputProps:
+                                    {
+                                        id: "inputWithdrawName",
+                                        useFloatingLabel: true,
+                                        floatingLabelText: "Item",
+                                        "data-statename": "Item"
+                                    },
+                                onChange: (e) => {
+                                    setTransactionData(e[0] ? {
+                                        ...transactionData,
+                                        name: e[0].name,
+                                        id: e[0].id,
+                                        selected: e,
+                                        displayQuantity: Math.max(Math.abs(transactionData.quantity), Number(maxQty)),
+                                        unit: e[0].unit
+                                    } : new transactionDataTemplate());
+                                    setMaxQty(formType === "withdraw" ? e[0]?.currentStock || null : null);
+                                },
+                                labelKey: "name",
+                                options: withdrawItemType === "item" ? itemList : listList,
+                                selected: transactionData.selected
+                            }}
+                        />
+                    </div>
+                    <div className="col-12 col-md-3 mb-3">
+                        <div className="row align-items-center">
+                            <div className="col-8"><FormInput type={"number"}
+                                                              id={"inputWithdrawQuantity"}
+                                                              label={"Quantity"}
+                                                              min={0}
+                                                              max={maxQty ? Math.max(0, maxQty) : null}
+                                                              onChange={(id, val) => {
+                                                                  const qty = maxQty ? Math.max(Math.min(maxQty, val), 0) * (formType === "withdraw" ? -1 : 1) : Math.max(val, 0) * (formType === "withdraw" ? -1 : 1);
+                                                                  setTransactionData({
+                                                                      ...transactionData,
+                                                                      displayQuantity: Math.abs(qty),
+                                                                      quantity: qty
+                                                                  });
+                                                              }}
+                                                              value={transactionData.displayQuantity}
+                            /></div>
+                            <div className="col-4"><p className="m-0">{transactionData.unit}</p></div>
                         </div>
-                    </div>}
-                    <div className={"col"}>
-                        <div className={"row align-items-center"}>
-                            <h5>{withdrawItemType === "item" ? "Item" : "List"}</h5>
-                        </div>
-                        <div className={"row align-items-center"}>
-                            <div className="col-12 col-md-2 mb-3">
-                                <p className="m-0">ID {transactionData.id}</p>
-                            </div>
-                            <div className="col-12 col-md-4 mb-3">
-                                <FormInput
-                                    type={"typeahead"} id={"inputWithdrawName"}
-                                    typeaheadProps={{
-                                        inputProps:
-                                            {
-                                                id: "inputRegisterOrganisation",
-                                                useFloatingLabel: true,
-                                                floatingLabelText: "Item",
-                                                "data-statename": "Item"
-                                            },
-                                        onChange: (e) => {
-                                            setTransactionData(e[0] ? {
-                                                ...transactionData,
-                                                name: e[0].name,
-                                                id: e[0].id,
-                                                selected: e,
-                                                displayQuantity: Math.max(Math.abs(transactionData.quantity), Number(maxQty)),
-                                                unit: e[0].unit
-                                            } : new transactionDataTemplate());
-                                            setMaxQty(formType === "withdraw" ? e[0]?.currentStock || null : null);
-                                        },
-                                        labelKey: "name",
-                                        options: withdrawItemType === "item" ? itemList : listList,
-                                        selected: transactionData.selected
-                                    }}
-                                />
-                            </div>
-                            <div className="col-md-4 mb-3">
-                                <FormInput type={"number"}
-                                           id={"inputWithdrawQuantity"}
-                                           label={"Quantity"}
-                                           min={0}
-                                           max={maxQty ? Math.max(0, maxQty) : null}
-                                           onChange={(id, val) => {
-                                               const qty = maxQty ? Math.max(Math.min(maxQty, val), 0) * (formType === "withdraw" ? -1 : 1) : Math.max(val, 0) * (formType === "withdraw" ? -1 : 1);
-                                               setTransactionData({
-                                                   ...transactionData,
-                                                   displayQuantity: Math.abs(qty),
-                                                   quantity: qty
-                                               });
-                                           }}
-                                           value={transactionData.displayQuantity}
-                                />
-                            </div>
-                            <div className="col-md-2 mb-3">
-                                <p className="m-0">{transactionData.unit}</p>
-                            </div>
-                        </div>
+                    </div>
+                    <div className="col-12 col-md-4 mb-3">
+                        <FormInput
+                            type={"typeahead"} id={"inputWithdrawLocation"}
+                            typeaheadProps={{
+                                inputProps:
+                                    {
+                                        id: "inputWithdrawLocation",
+                                        useFloatingLabel: true,
+                                        floatingLabelText: "Location",
+                                        "data-statename": "Location"
+                                    },
+                                onChange: (e) => {
+                                    setTransactionData({
+                                        ...transactionData,
+                                        locationId: e[0]?.id || null,
+                                    });
+                                },
+                                labelKey: "name",
+                                options: mockLocations.sort((a, b) => {
+                                    return naturalSort(a.name, b.name)
+                                }),
+                                // selected: transactionData.selected
+                            }}
+                        />
                     </div>
                 </div>
                 <div className={"row"}>
@@ -188,7 +197,8 @@ const TransactionForm = ({formType}) => {
                 </div>
             </form>
         </div>
-    );
+    )
+        ;
 };
 
 TransactionForm.propTypes = {
