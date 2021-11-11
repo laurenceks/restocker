08e0b09b8eed0b5585fb3d2d97a780fc00d141ee
Laurence Summers <lks@live.co.uk>
2021-11-11 18:33:59 +0000

    Added safeguard for making sure there is sufficient stock before commiting to a withdraw

--- public/php/items/addTransaction.php
+++ public/php/items/addTransaction.php
@@ -3,10 +3,38 @@ require "../security/userLoginSecurityCheck.php";
 require "../security/userAdminRightsCheck.php";
 require_once "../common/db.php";
 require_once "../common/updateCurrentStockLevel.php";
+require "../common/fetchFunctions/fetchFunctionItems.php";
 
 $input = json_decode(file_get_contents('php://input'), true);
 
-$output = array("success" => false, "feedback" => "An unknown error occurred");
+$output = array("success" => false, "feedback" => "An unknown error occurred", "outOfStockItems" => array(), "errorType" => null);
+
+$currentItemsAtLocation = array_filter(fetchFunctionItems($_SESSION["user"]->organisationId, $input["locationId"]), function ($row) use ($input, $output) {
+    return intval($row["locationId"]) == $input["locationId"];
+});
+
+
+if ($input["transactionType"] !== "restock") {
+    $currentItemsAtLocationByItemId = array();
+    foreach ($currentItemsAtLocation as $item) {
+        $currentItemsAtLocationByItemId[$item["id"]] = $item;
+    }
+    $output["current"] = $currentItemsAtLocationByItemId;
+
+    //make sure each item has sufficient stock before proceeding
+    foreach ($input["transactionArray"] as $transaction) {
+        if (!$transaction["itemId"] || !$currentItemsAtLocationByItemId[$transaction["itemId"]] || abs($transaction["quantity"]) > $currentItemsAtLocationByItemId[$transaction["itemId"]]["currentStock"]) {
+            //an item can't be withdrawn
+            $output["outOfStockItems"][] = array("id" => $transaction["itemId"], "requested" => abs($transaction["quantity"]), "current" => $currentItemsAtLocationByItemId[$transaction["itemId"]]["currentStock"]);
+        }
+    }
+    if (count($output["outOfStockItems"]) > 0) {
+        $output["feedback"] = "There is insufficient stock for " . (count($output["outOfStockItems"]) === 1 ? "one" : "some") . " of the items requested, please try again";
+        $output["errorType"] = "outOfStock";
+        echo json_encode($output);
+        exit();
+    }
+}
 
 try {
     $addTransaction = $db->prepare("INSERT INTO transactions (itemId, type, quantity, userId, organisationId, locationId) VALUES (:itemId, :type, :quantity, :userId, :organisationId, :locationId)");
@@ -20,8 +48,7 @@ try {
     $addTransaction->execute();
     $output["success"] = true;
     $output["feedback"] = "Transaction added (" . $transactionType . ")";
-
-    $output["stockLevelUpdate"] = updateCurrentStockLevel();
+    updateCurrentStockLevel();
 
 } catch
 (PDOException $e) {
---
+++ public/php/items/getAllItemsAndStockLevels.php
@@ -0,0 +1,45 @@
+<?php
+require "../security/userLoginSecurityCheck.php";
+require "../security/userAdminRightsCheck.php";
+require "../common/db.php";
+
+$output = array("items" => array());
+$getAllItems = $db->prepare('SELECT items.id,
+                                   items.name,
+                                   items.unit,
+                                   "all" AS locationId,
+                                   items.currentStock,
+                                   items.warninglevel
+                            FROM   items
+                                   LEFT JOIN transactions
+                                          ON items.id = transactions.itemid
+                            GROUP  BY transactions.itemid
+                            UNION
+                            SELECT items.id,
+                                   items.name,
+                                   items.unit,
+                                   transactions.locationid,
+                                   CAST(SUM(quantity) AS INTEGER) AS currentStock,
+                                   items.warninglevel
+                            FROM   `items`
+                                   LEFT JOIN transactions
+                                          ON items.id = transactions.itemid
+                            WHERE  items.organisationid = :organisationId1
+                                   AND transactions.organisationid = :organisationId2
+                            GROUP  BY items.id,
+                                      transactions.locationid
+                            ORDER  BY locationid,
+                                      id; ');
+$getAllItems->bindValue(':organisationId1', $_SESSION["user"]->organisationId);
+$getAllItems->bindValue(':organisationId2', $_SESSION["user"]->organisationId);
+$getAllItems->execute();
+$items = $getAllItems->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($items as $row) {
+    $output["itemsByLocationId"][$row["locationId"]][] = $row;
+    if ($row["locationId"] === "all") {
+        $output["items"][] = $row;
+    }
+};
+
+echo json_encode($output);
