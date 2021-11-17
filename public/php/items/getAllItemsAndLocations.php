f50e732bf94e364dfa851fbcfc54235118acab0e
Laurence Summers <lks@live.co.uk>
2021-11-17 19:30:53 +0000

    Added list retrieval

--- public/php/items/getAllItemsAndLocations.php
+++ public/php/items/getAllItemsAndLocations.php
@@ -4,6 +4,7 @@ require "../security/userAdminRightsCheck.php";
 require "../common/db.php";
 require "../common/fetchFunctions/fetchFunctionLocations.php";
 require "../common/fetchFunctions/fetchFunctionItems.php";
+require "../common/fetchFunctions/fetchFunctionLists.php";
 
 $output = array("items" => array(), "locations" => array(), "itemsByLocationId" => array());
 $items = fetchFunctionItems($_SESSION["user"]->organisationId);
@@ -14,6 +15,29 @@ foreach ($items as $row) {
     if ($row["locationId"] === "all") {
         $output["items"][] = $row;
     }
+};
+
+$lists = fetchFunctionLists($_SESSION["user"]->organisationId);
+
+$previousRow = array("locationId" => null, "id" => null);
+foreach ($lists as $row) {
+    if ($previousRow["locationId"] === $row["locationId"] && $previousRow["id"] === $row["id"]) {
+        $rowItem = array_slice($row, 3);
+        $listsTypes = array(
+            &$output["lists"][count($output["lists"]) - 1],
+            &$output["listsByLocationId"][$row["locationId"]][count($output["listsByLocationId"][$row["locationId"]]) - 1],
+            &$output["listsByLocationThenListId"][$row["locationId"]][$row["id"]]);
+        foreach ($listsTypes as &$prev) {
+            $prev["items"][] = $rowItem;
+            $prev["currentStock"] = min($prev["currentStock"], floor($row["currentStock"] / $row["quantity"]));
+        }
+    } else {
+        $rowStart = array("id" => $row["id"], "name" => $row["listName"], "locationId" => $row["locationId"], "currentStock" => floor($row["currentStock"] / $row["quantity"]), "items" => array(array_slice($row, 3)));
+        $output["lists"][] = $rowStart;
+        $output["listsByLocationId"][$row["locationId"]][] = $rowStart;
+        $output["listsByLocationThenListId"][$row["locationId"]][$row["id"]] = $rowStart;
+    }
+    $previousRow = $row;
 };
 
 $output["locations"] = fetchFunctionLocations($_SESSION["user"]->organisationId);
