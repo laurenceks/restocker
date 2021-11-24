0a2699fe172b9cb0243255dd04f3bbcd07e5547a
Laurence Summers <lks@live.co.uk>
2021-11-24 20:06:10 +0000

    Removed code now in TableCell, added support for row mouse enter/leave events, added rowspan support, added cell align to TableCell

--- src/components/common/tables/Table.js
+++ src/components/common/tables/Table.js
@@ -1,42 +1,7 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-import {IoCheckmarkCircleSharp, IoCloseCircleSharp} from "react-icons/all";
-import FormInput from "../forms/FormInput";
 import TableCell from "./TableCell";
 
-const renderCellContent = (x) => {
-    if (!x) {
-        return "";
-    } else if (!x.type) {
-        return typeof x === "string" || typeof x === "number" ? x : "";
-    } else {
-        switch (x.type) {
-            case "tick":
-            case "tick-invert":
-                return (
-                    <span
-                        className={`tableIcon ${x.type !== "tick-invert" ? (x.value ? "text-success" : "text-danger") : !x.value ? "text-success" : "text-danger"}`}>
-                        {x.value ? <IoCheckmarkCircleSharp/> :
-                            <IoCloseCircleSharp/>}
-                    </span>
-                )
-            case "button":
-            case "submit":
-                return (
-                    <button type={x.type === "submit" && "submit"} className={`btn ${x.buttonClass || "btn-primary"}`}
-                            onClick={x.handler} data-userid={x.id}>{x.text}</button>
-                )
-            case "input":
-                return (
-                    <FormInput {...x.props}/>
-                )
-            default:
-                return x.text || "";
-        }
-    }
-}
-
-const Table = ({title, headers, rows, tableClassName, fullWidth}) => {
+const Table = ({title, headers, rows, tableClassName, fullWidth, rowEnter, rowLeave}) => {
     return (
         <div className={`table-responsive ${fullWidth && "w-100"}`}>{(rows && rows.length > 0) ?
             <table className={`table table-hover ${tableClassName}`}>
@@ -46,6 +11,7 @@ const Table = ({title, headers, rows, tableClassName, fullWidth}) => {
                         if (x) {
                             return (<th key={`${title}-th-${i}`}
                                         colSpan={x.colspan}
+                                        rowSpan={x.rowspan}
                                         className={x.className}>
                                 {x.text || x}</th>)
                         }
@@ -55,11 +21,14 @@ const Table = ({title, headers, rows, tableClassName, fullWidth}) => {
                 <tbody>
                 {rows.map((x, i) => {
                     return (
-                        <tr key={`${title}-tr-${i}`}>
+                        <tr key={`${title}-tr-${i}`} onMouseEnter={rowEnter} onMouseLeave={rowLeave}>
                             {x.map((y, j) => {
                                 if (y) {
-                                    return <TableCell key={`${title}-tr-${i}-td-${j}`} content={y}
-                                                      className={y?.className}/>
+                                    return <TableCell key={`${title}-tr-${i}-td-${j}`}
+                                                      content={y}
+                                                      className={y?.className}
+                                                      align={y?.cellAlignClass}
+                                    />
                                 }
                             })}
                         </tr>
@@ -77,7 +46,9 @@ Table.propTypes = {
     tableClassName: PropTypes.string,
     headers: PropTypes.array,
     rows: PropTypes.array,
-    fullWidth: PropTypes.bool
+    fullWidth: PropTypes.bool,
+    rowEnter: PropTypes.func,
+    rowLeave: PropTypes.func
 };
 
 Table.defaultProps = {
@@ -85,7 +56,9 @@ Table.defaultProps = {
     tableClassName: null,
     headers: [],
     rows: [],
-    fullWidth: false
+    fullWidth: false,
+    rowEnter: null,
+    rowLeave: null
 }
 
 export default Table;
