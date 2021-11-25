319b0228f5f25b14d348e7cc6c16dd60ad9e40ac
Laurence Summers <lks@live.co.uk>
2021-11-25 19:19:57 +0000

    Moved state construciton into functions which are called each time the listList state updates, this prevents stale references in callbacks, also added FormItem components to handle item selection

--- src/components/lists/Lists.js
+++ src/components/lists/Lists.js
@@ -7,41 +7,48 @@ import fetchAllItems from "../../functions/fetchAllItems";
 import naturalSort from "../../functions/naturalSort";
 import {log10} from "chart.js/helpers";
 import deepmerge from "deepmerge";
+import FormItem from "../common/forms/FormItem";
 
 const Lists = () => {
         class addDataTemplate {
             constructor() {
                 this.name = "";
-                this.items = [];
+                this.itemId = null;
+                this.unit = null;
+                this.quantity = "";
             }
         }
 
         const [addData, setAddData] = useState(new addDataTemplate());
-        const [editId, setEditId] = useState(null);
         const [sortKey, setSortKey] = useState("name");
         const [listList, setListList] = useState([]);
         const [listsById, setListsById] = useState([]);
         const [listRows, setListRows] = useState([]);
         const addListForm = useRef();
         const listRowsRef = useRef(listRows);
+        const listsRef = useRef(listList);
         listRowsRef.current = listRows;
+        listRowsRef.current = listList;
 
         class editListDataTemplate {
             constructor() {
                 this.id = null;
+
                 this.name = null;
                 this.items = [];
+                this.index = null;
             }
         }
 
         const [editedList, setEditedList] = useState(new editListDataTemplate());
+        const editedListRef = useRef(editedList);
+        editedListRef.current = editedList;
 
         const getLists = (callback, getLocations = false) => {
             setEditedList(new editListDataTemplate());
-            console.log("getting")
             fetchJson("./php/lists/getAllLists.php", {
                 method: "GET"
-            }, processLists);
+            }, x => setListList(x?.lists || []));
         }
 
         useEffect(() => {
@@ -51,59 +58,62 @@ const Lists = () => {
             })
         }, [listRows, editedList]);
 
+        useEffect(() => {
+            setListRows(makeListRows(listList))
+        }, [listList]);
 
-        const processLists = (x) => {
-            setListList(x?.lists || null);
-            const newListRows = []
-            const newListsById = {};
-            x?.lists?.forEach((x) => {
-                newListsById[x.id] = x;
+
+        const makeListRows = (lists = listList || [], editListIndex = editedList.index === 0 ? 0 : editedList.index || -1) => {
+            const newListRows = [];
+            lists.forEach((x, i) => {
                 const cellTemplate = {cellData: {"data-listid": x.id}, className: `td-listId-${x.id}`};
-                x.items.forEach((y, i) => {
-                    const currentLength = newListRows.length;
-                    newListRows.push((i === 0 ? [
-                        {
+                if (i !== editListIndex) {
+                    x.items.forEach((y, j) => {
+                        newListRows.push((j === 0 ? [
+                            {
+                                ...cellTemplate,
+                                rowspan: x.items.length,
+                                text: x.id
+                            },
+                            {
+                                ...cellTemplate,
+                                rowspan: x.items.length,
+                                text: x.name,
+                            }] : []).concat(
+                            [{
+                                ...cellTemplate,
+                                text: y.itemName
+                            }, {
+                                ...cellTemplate,
+                                text: `${y.quantity} ${y.unit}`
+                            }]).concat(j === 0 && editListIndex === -1 ? [{
                             ...cellTemplate,
+                            type: "button",
+                            text: "Edit",
+                            buttonClass: "btn-warning",
                             rowspan: x.items.length,
-                            text: x.id
-                        },
-                        {
-                            ...cellTemplate,
-                            rowspan: x.items.length,
-                            text: x.name,
-                        }] : []).concat(
-                        [{
-                            ...cellTemplate,
-                            text: y.itemName
+                            className: "text-center " + cellTemplate.className,
+                            handler: () => {
+                                setEditedList({...x, index: i});
+                                setListRows(makeListRows(listList, i));
+                            }
                         }, {
                             ...cellTemplate,
-                            text: y.quantity
-                        }]).concat(i === 0 ? [{
-                        ...cellTemplate,
-                        type: "button",
-                        text: "Edit",
-                        buttonClass: "btn-warning",
-                        rowspan: x.items.length,
-                        className: "text-center " + cellTemplate.className,
-                        handler: () => {
-                            setEditedList(x);
-                            showEditRow(x, currentLength);
-                        }
-                    }, {
-                        ...cellTemplate,
-                        type: "button",
-                        text: "Delete",
-                        buttonClass: "btn-danger",
-                        rowspan: x.items.length,
-                        className: "text-center " + cellTemplate.className,
-                        handler: () => {
-                            console.log("Delete!");
-                        }
-                    }] : []));
-                });
+                            type: "button",
+                            text: "Delete",
+                            buttonClass: "btn-danger",
+                            rowspan: x.items.length,
+                            className: "text-center " + cellTemplate.className,
+                            handler: () => {
+                                deleteList(x.id);
+                            }
+                        }] : j === 0 ? [{...cellTemplate, text: "", rowspan: x.items.length, colspan: 2}] : null));
+                    })
+                } else {
+                    newListRows.push(...makeEditRow(x, i))
+                }
             })
-            setListsById(newListsById);
-            setListRows(newListRows);
+            return newListRows;
         }
 
         const addList = (x) => {
@@ -133,71 +143,38 @@ const Lists = () => {
             });
         }
 
-        const showEditRow = (x, start) => {
-            setEditedList({id: x.id, name: x.name, items: x.items})
+        const makeEditRow = (currentList, listIndex) => {
             const cellTemplate = {
-                cellData: {"data-listid": x.id},
-                className: `td-listId-${x.id}`,
+                cellData: {"data-listid": currentList.id},
+                className: `td-listId-${currentList.id}`,
                 cellAlignClass: "align-top"
             };
             const newEditRow = [];
             const firstRow = [
-                {...cellTemplate, text: x.id, rowspan: x.items?.length + 1 || 1},
+                {...cellTemplate, text: currentList.id, rowspan: currentList.items?.length + 1 || 1},
                 {
                     ...cellTemplate,
-                    rowspan: x.items?.length + 1 || 1,
+                    rowspan: currentList.items?.length + 1 || 1,
                     type: "input",
                     props: {
                         type: "text",
                         label: "Name",
-                        id: `input-listId-${x.id}-name`,
-                        defaultValue: x.name,
+                        id: `input-listId-${currentList.id}-name`,
+                        defaultValue: currentList.name,
                         onChange: (e, v) => {
-                            setEditedList({...editedList, name: v})
+                            setEditedList(prevState => {
+                                return {...prevState, name: v}
+                            });
                         }
                     }
                 }];
-            x.items.forEach((y, i) => {
+            currentList.items.forEach((y, i) => {
                     newEditRow.push((i === 0 ? firstRow : []).concat(
-                        [{
-                            ...cellTemplate,
-                            type: "input",
-                            props: {
-                                label: "Item",
-                                id: `input-listId-${x.id}-itemId-${y.itemId}-name`,
-                                defaultValue: x.items[i]?.itemName,
-                                onChange: (e, v) => {
-                                    x.items.splice(i, 1, {
-                                        ...x.items[i],
-                                        name: v
-                                    })
-                                    setEditedList({...editedList, items: x.items})
-                                }
-                            }
-                        }
-                            , {
-                            ...cellTemplate,
-                            type: "input",
-                            props: {
-                                label: "Quantity",
-                                type: "Number",
-                                id: `input-listId-${x.id}-itemId-${y.itemId}-quantity`,
-                                defaultValue: x.items[i]?.quantity,
-                                onChange: (e, v) => {
-                                    x.items.splice(i, 1, {
-                                        ...x.items[i],
-                                        quantity: v
-                                    })
-                                    setEditedList({...editedList, items: x.items})
-                                }
-                            }
-                        }
-                        ])
+                            makeInputCells(currentList, y, i, cellTemplate, listIndex)
+                        )
                     )
-                    ;
                 }
             )
-            ;
             newEditRow.push([{
                 ...cellTemplate,
                 type: "button",
@@ -205,25 +182,108 @@ const Lists = () => {
                 buttonClass: "btn-primary",
                 className: cellTemplate.className,
                 handler: () => {
-                    console.log("Add new item!");
+                    const newItems = [...currentList.items];
+                    newItems.push({
+                        itemId: null,
+                        itemName: null,
+                        unit: "units",
+                        quantity: null,
+                        listItemsId: currentList.id
+                    });
+                    //save new item in edited list
+                    setEditedList((prevState) => {
+                        return {...prevState, items: newItems}
+                    });
+                    setListList((x) => {
+                        x[listIndex].items = newItems;
+                        return [...x];
+                    })
                 }
             }, {
                 ...cellTemplate,
+                colspan: 3,
                 fragment: <Fragment>
-                    <button className="btn btn-success me-3" onClick={() => console.log("Save")}>Save</button>
+                    <button className="btn btn-success me-3" onClick={() => editList(editedListRef.current)}>Save</button>
                     <button className="btn btn-danger" onClick={() => getLists()}>Cancel</button>
                 </Fragment>,
                 className: cellTemplate.className,
             }]);
-            const newListRows = [...listRowsRef.current].map((x) => {
-                return x.map((y) => {
-                    //remove buttons from all cells
-                    return y.type === "button" ? "" : y;
-                })
-            });
-            newListRows.splice(start, newEditRow.length - 1, ...newEditRow);
-            setListRows(newListRows);
+            return newEditRow;
         }
+
+        const makeInputCells = (x, y, i, cellTemplate = {}, startIndex = 0) => {
+            const rowIndex = startIndex + i;
+            console.log([{...x.items[i], name: x.items[i].itemName, id: x.items[i].itemId}]);
+            return [{
+                ...cellTemplate,
+                fragment: <FormItem id={`input-listId-${x.id}-itemId-${y.itemId}-name`}
+                                    label={"Item"}
+                                    invalidFeedback={"You must select an item from the list"}
+                                    defaultSelected={[{...x.items[i], name: x.items[i].itemName, id: x.items[i].itemId}]}
+                                    filterValues={{key: "id", values: x.items.map(x => x.itemId)}}
+                                    onChange={(e) => {
+                                        x.items.splice(i, 1, {
+                                            ...x.items[i],
+                                            itemName: e?.[0]?.name || null,
+                                            itemId: e?.[0]?.id || null,
+                                            unit: e?.[0]?.unit || null,
+                                        })
+                                        //update the editedList with new items
+                                        setEditedList(prevState => {
+                                            return {...prevState, items: x.items}
+                                        });
+                                        //update the full list so the table is re-rendered
+                                        setListList((prevState) => {
+                                            prevState[startIndex] = x;
+                                            return [...prevState];
+                                        })
+                                    }}
+                />
+            }
+                , {
+                    ...cellTemplate,
+                    type: "input",
+                    props: {
+                        label: "Quantity",
+                        type: "Number",
+                        id: `input-listId-${x.id}-itemId-${y.itemId}-quantity`,
+                        defaultValue: x.items[i]?.quantity || null,
+                        onChange: (e, v) => {
+                            x.items.splice(i, 1, {
+                                ...x.items[i],
+                                quantity: v
+                            })
+                            setEditedList(prevState => {
+                                return {...prevState, items: x.items}
+                            })
+                        }
+                    }
+                }, {
+                    ...cellTemplate,
+                    className: "align-middle " + cellTemplate.className,
+                    text: x.items[i]?.unit || "units"
+                },
+                x.items.length > 1 ? {
+                    ...cellTemplate,
+                    type: "button",
+                    text: "Delete",
+                    buttonClass: "btn-danger",
+                    className: "align-middle " + cellTemplate.className,
+                    handler: () => {
+                        const newDeleteItems = [...listList[startIndex].items];
+                        newDeleteItems.splice(i, 1);
+                        setEditedList((prevState) => {
+                            return {...prevState, items: newDeleteItems}
+                        })
+                        setListList((x) => {
+                            x[startIndex].items = newDeleteItems;
+                            return [...x];
+                        })
+                    }
+                } : ""
+            ]
+        }
+
 
         useEffect(() => {
             //get list list
@@ -240,13 +300,37 @@ const Lists = () => {
                         <div className="col-12 col-md-4 mb-3 mb-md-0">
                             <div className="loginFormInputGroup">
                                 <FormInput type={"text"}
-                                           id={"inputAddItemName"}
+                                           id={"inputAddListName"}
                                            label={"Name"}
-                                           invalidFeedback={"You must name your item"}
+                                           invalidFeedback={"You must name your list"}
                                            forceCase={"title"}
                                            value={addData.name}
                                            onChange={(e, x) => {
                                                setAddData({...addData, name: x})
+                                           }}/>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-4 mb-3 mb-md-0">
+                            <div className="loginFormInputGroup">
+                                <FormItem id={"inputAddListItem"}
+                                          label={"Item"}
+                                          invalidFeedback={"You must select an item from the list"}
+                                          onChange={(e) => {
+                                              setAddData({...addData, itemId: e[0]?.id || null, unit: e[0]?.unit || null})
+                                          }}
+                                />
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-2 mb-3 mb-md-0">
+                            <div className="loginFormInputGroup">
+                                <FormInput type={"number"}
+                                           id={"inputAddListItemQuantity"}
+                                           label={"Quantity"}
+                                           invalidFeedback={"You must enter an item quantity"}
+                                           min={0}
+                                           value={addData.quantity}
+                                           onChange={(e, x) => {
+                                               setAddData({...addData, quantity: x})
                                            }}/>
                             </div>
                         </div>
