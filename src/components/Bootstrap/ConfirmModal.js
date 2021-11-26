6035ad302f1eac8542a2e01acaa058d1e2d94cf3
Laurence Summers <lks@live.co.uk>
2021-11-26 10:18:02 +0000

    Added styling props to modal

--- src/components/Bootstrap/ConfirmModal.js
+++ src/components/Bootstrap/ConfirmModal.js
@@ -1,19 +1,19 @@
 import PropTypes from 'prop-types';
 import {Button, Modal} from "react-bootstrap";
 
-const ConfirmModal = ({show, title, bodyText, handleYes, handleNo, yesText, noText}) => {
+const ConfirmModal = ({show, title, bodyText, handleYes, handleNo, yesText, noText, headerClass, yesButtonVariant, noButtonVariant}) => {
     return (
         <Modal show={show} onHide={handleNo} backdrop="static" aria-labelledby="contained-modal-title-vcenter"
                centered>
-            <Modal.Header closeButton>
+            <Modal.Header className={headerClass}>
                 <Modal.Title>{title}</Modal.Title>
             </Modal.Header>
             <Modal.Body>{bodyText}</Modal.Body>
             <Modal.Footer>
-                <Button variant="secondary" onClick={handleNo}>
+                <Button variant={noButtonVariant} onClick={handleNo}>
                     {noText}
                 </Button>
-                <Button variant="primary" onClick={handleYes}>
+                <Button variant={yesButtonVariant} onClick={handleYes}>
                     {yesText}
                 </Button>
             </Modal.Footer>
@@ -25,24 +25,30 @@ ConfirmModal.propTypes = {
     show: PropTypes.bool,
     handleYes: PropTypes.func,
     handleNo: PropTypes.func,
-    yesText: PropTypes.string,
-    noText: PropTypes.string,
     bodyText: PropTypes.string,
-    title: PropTypes.string
+    headerClass: PropTypes.string,
+    noText: PropTypes.string,
+    noButtonVariant: PropTypes.string,
+    title: PropTypes.string,
+    yesText: PropTypes.string,
+    yesButtonVariant: PropTypes.string,
 };
 
 ConfirmModal.defaultProps = {
     show: false,
-    handleYes: (e) => {
-        console.log("Yes clicked, but no handler passed")
-    },
+    headerClass: null,
     handleNo: (e) => {
         console.log("No/cancel clicked, but no handler passed")
     },
-    yesText: "Yes",
+    handleYes: (e) => {
+        console.log("Yes clicked, but no handler passed")
+    },
+    bodyText: "Are you sure you wish to proceed?",
     noText: "Cancel",
+    noButtonVariant: "secondary",
     title: "Are you sure?",
-    bodyText: "Are you sure you wish to proceed?"
+    yesText: "Yes",
+    yesButtonVariant: "primary",
 }
 
 export default ConfirmModal;
