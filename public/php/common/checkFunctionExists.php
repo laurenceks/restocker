3568f44b9a9942da2c613064a698b5f92f5412f1
Laurence Summers <lks@live.co.uk>
2021-11-26 19:10:56 +0000

    Added function for checking if any entries exist for given conditions

---
+++ public/php/common/checkFunctionExists.php
@@ -0,0 +1,38 @@
+<?php
+function checkFunctionExists($table, $selectKey, $keyValues, $noDeletedColumn = false)
+{
+    require_once "../security/userLoginSecurityCheck.php";
+    require "../common/db.php";
+
+    $result = false;
+    $whiteList = array(
+        "tables" => array("lists", "list_items", "items", "locations", "transactions"),
+        "keys" => array("id", "name", "listId", "itemId")
+    );
+
+    if (count($keyValues) > 0 && in_array($table, $whiteList["tables"], true) && in_array($selectKey, $whiteList["keys"], true)) {
+        $whereString = " WHERE";
+        $i = 1;
+        $j = 0;
+        $k = 1;
+        foreach ($keyValues as $pair) {
+            if (in_array($pair["key"], $whiteList["keys"], true)) {
+                $whereString .= ($j === 0 ? " " : " AND ") . $pair["key"] . " = :value" . $i;
+                $j++;
+            }
+            $i++;
+        }
+
+        if ($j > 0) {
+            $check = $db->prepare("SELECT " . $selectKey . " FROM " . $table . $whereString . ($noDeletedColumn ? "" : " AND deleted = 0"));
+            foreach ($keyValues as $pair) {
+                $check->bindValue(':value' . $k, $pair["value"]);
+                $k++;
+            }
+            $check->execute();
+            $result = count($check->fetchAll(PDO::FETCH_ASSOC)) > 0;
+        }
+    }
+
+    return $result;
+}
