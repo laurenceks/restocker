bad50567f06f1c354973e55fb883f2c64fd6f08b
Laurence Summers <lks@live.co.uk>
2021-12-04 10:48:32 +0000

    Re-structured data flow to make itemList one point of truth for current items

--- src/components/items/Items.js
+++ src/components/items/Items.js
@@ -8,128 +8,89 @@ import naturalSort from "../../functions/naturalSort";
 import ConfirmModal from "../Bootstrap/ConfirmModal";
 
 const Items = () => {
-    class addDataTemplate {
-        constructor() {
-            this.name = "";
-            this.unit = [];
-            this.warningLevel = 5;
+        const addDataTemplate = {
+            name: "",
+            unit: "units",
+            warningLevel: 5
         }
-    }
 
-    const [addData, setAddData] = useState(new addDataTemplate());
-    const [editId, setEditId] = useState(null);
-    //TODO add sort key to table headers on click
-    const [sortKey, setSortKey] = useState("name");
-    const [itemList, setItemList] = useState([[0, "", "", "", "", ""]]);
-    const [modalOptions, setModalOptions] = useState({
-        show: false,
-        deleteId: null,
-        bodyText: "",
-        headerClass: "bg-danger text-light",
-        yesButtonVariant: "danger"
-    });
+        const [addItemData, setAddItemData] = useState({...addDataTemplate});
+        const [editId, setEditId] = useState(null);
+        //TODO add sort key to table headers on click
+        const [sortKey, setSortKey] = useState("name");
+        const [itemList, setItemList] = useState([]);
+        const [modalOptions, setModalOptions] = useState({
+            show: false,
+            deleteId: null,
+            bodyText: "",
+            headerClass: "bg-danger text-light",
+            yesButtonVariant: "danger"
+        });
 
-    const addItemForm = useRef();
-    const itemListRef = useRef([]);
+        const addItemForm = useRef();
 
-    itemListRef.current = itemList;
+        const getItems = () => {
+            setEditId(null);
+            fetchAllItems((result) => setItemList(result.items))
+        }
 
-    const getItems = () => {
-        fetchAllItems(processItems)
-    }
-
-    const processItems = (x) => {
-        const newItemsList = []
-        x.items.map((x) => {
-            return {...x, sortKey: x[sortKey]}
-        }).sort(naturalSort).forEach((item, index) => {
-                newItemsList.push(
-                    [
-                        item.id,
-                        item.name,
-                        `${item.currentStock} ${item.unit}`,
-                        `${item.warningLevel} ${item.unit}`,
-                        {
-                            type: "button",
-                            id: 1,
-                            text: "Edit",
-                            buttonClass: "btn-warning btn-sm",
-                            handler: () => {
-                                showEditRow(index);
+        const makeItemRows = () => {
+            return itemList.map((x) => {
+                return {...x, sortKey: x[sortKey]}
+            }).sort(naturalSort).map(item => {
+                    return (
+                        item.id !== editId ? [
+                            item.id,
+                            item.name,
+                            `${item.currentStock} ${item.unit}`,
+                            `${item.warningLevel} ${item.unit}`,
+                            {
+                                type: "button",
+                                id: 1,
+                                text: "Edit",
+                                buttonClass: "btn-warning btn-sm",
+                                handler: () => {
+                                    setEditId(item.id)
+                                }
+                            },
+                            {
+                                type: "button",
+                                id: 1,
+                                text: "Delete",
+                                buttonClass: "btn-danger btn-sm",
+                                handler: () => {
+                                    setModalOptions(prevState => {
+                                        return {
+                                            ...prevState,
+                                            show: true,
+                                            deleteId: item.id,
+                                            bodyText: `Are you sure you want to delete ${item.name}?\n\nThe item will also be removed from any lists containing it.`
+                                        }
+                                    })
+                                }
                             }
-                        },
-                        {
-                            type: "button",
-                            id: 1,
-                            text: "Delete",
-                            buttonClass: "btn-danger btn-sm",
-                            handler: () => {
-                                setModalOptions(prevState => {
-                                    return {
-                                        ...prevState,
-                                        show: true,
-                                        deleteId: item.id,
-                                        bodyText: `Are you sure you want to delete ${item.name}?\n\nThe item will also be removed from any lists containing it.`
-                                    }
-                                })
-                            }
-                        }
-                    ]
-                )
-            }
-        )
-        setItemList(newItemsList);
-    }
+                        ] : makeEditRow(item)
+                    )
+                }
+            )
+        }
 
-    const addItem = (form) => {
-        fetchJson("./php/items/addItem.php", {
-            method: "POST",
-            body: JSON.stringify(form.values),
-        }, (x) => {
-            setAddData(new addDataTemplate());
-            getItems();
-        });
-    }
-
-    const deleteItem = (id) => {
-        fetchJson("./php/items/deleteItem.php", {
-            method: "POST",
-            body: JSON.stringify({id: id}),
-        }, (x) => {
-            setModalOptions(prevState => {
-                return {...prevState, show: false}
-            })
-            getItems();
-        });
-    }
-    const editItem = (form) => {
-        fetchJson("./php/items/editItem.php", {
-            method: "POST",
-            body: JSON.stringify(form),
-        }, (x) => {
-            getItems();
-        });
-    }
-
-    const showEditRow = (x) => {
-        if (itemListRef.current[x]) {
-            const newItemList = [...itemListRef.current];
-            const unitText = itemListRef.current[x][2].match(/\d+ (.*)/)
-            const itemId = itemListRef.current[x][0];
+        const makeEditRow = (item) => {
+            console.log(item);
             const inputIds = {
                 name: `editItemRow-${editId}-name`,
                 unit: `editItemRow-${editId}-unit`,
                 warningLevel: `editItemRow-${editId}-wanringLevel`
-            }
-            newItemList[x] = [
-                itemId,
+            };
+            return [
+                item.id,
                 {
                     type: "input",
                     props: {
                         type: "text",
                         id: inputIds.name,
                         label: "Item name",
-                        defaultValue: itemListRef.current[x][1],
+                        defaultValue: item.name,
                         form: "editItemForm",
                         forceCase: "title"
                     },
@@ -140,7 +101,7 @@ const Items = () => {
                         type: "text",
                         id: inputIds.unit,
                         label: "Unit name",
-                        defaultValue: unitText && unitText[1] ? unitText[1].trim() : "",
+                        defaultValue: item.unit && item.unit ? item.unit.trim() : "",
                         form: "editItemForm",
                         forceCase: "lower"
                     },
@@ -151,7 +112,7 @@ const Items = () => {
                         type: "number",
                         id: inputIds.warningLevel,
                         label: "Warning level",
-                        defaultValue: 5,
+                        defaultValue: item.warningLevel,
                         form: "editItemForm",
                     },
                     invalidFeedback: "You must specify a warning level"
@@ -159,106 +120,146 @@ const Items = () => {
                     type: "submit",
                     buttonClass: "btn-success",
                     text: "Save",
-                    id: itemId,
+                    id: item.id,
                     className: "text-center buttonCell",
                     form: "editItemForm",
                     handler: (e) => {
-                        setEditId(itemId);
                         validateForm(e, [inputIds.name, inputIds.unit, inputIds.warningLevel], (x) => {
-                            editItem({
-                                name: x.values[inputIds.name],
-                                unit: x.values[inputIds.unit],
-                                warningLevel: x.values[inputIds.warningLevel],
-                                id: itemId
-                            })
+                            if (x.isValid) {
+                                editItem({
+                                    name: x.values[inputIds.name],
+                                    unit: x.values[inputIds.unit],
+                                    warningLevel: x.values[inputIds.warningLevel],
+                                    id: item.id
+                                })
+                            }
                         })
                     }
                 }, {
                     type: "button",
                     buttonClass: "btn-danger",
                     text: "Cancel",
-                    id: itemId,
+                    id: item.id,
                     className: "text-center buttonCell",
                     handler: getItems
-                }];
-            setItemList(newItemList);
+                }]
         }
-    }
 
-    useEffect(() => {
-        //get user list
-        getItems();
-    }, []);
+        const addItem = (form) => {
+            fetchJson("./php/items/addItem.php", {
+                method: "POST",
+                body: JSON.stringify(form.values),
+            }, () => {
+                setAddItemData({...addDataTemplate});
+                getItems();
+            });
+        }
 
-    return (
-        <div className="container">
-            <form ref={addItemForm} onSubmit={(e) => {
-                validateForm(e, addItemForm, addItem);
-            }}>
+        const deleteItem = (id) => {
+            fetchJson("./php/items/deleteItem.php", {
+                method: "POST",
+                body: JSON.stringify({id: id}),
+            }, (x) => {
+                setModalOptions(prevState => {
+                    return {...prevState, show: false}
+                })
+                getItems();
+            });
+        }
+
+        const editItem = (form) => {
+            fetchJson("./php/items/editItem.php", {
+                method: "POST",
+                body: JSON.stringify(form),
+            }, () => {
+                getItems();
+            });
+        }
+
+        useEffect(() => {
+            getItems();
+        }, []);
+
+        useEffect(() => {
+            makeItemRows();
+        }, [editId]);
+
+        return (
+            <div className="container">
+                <form ref={addItemForm} onSubmit={(e) => {
+                    validateForm(e, addItemForm, addItem);
+                }}>
+                    <div className="row my-3">
+                        <h2>Add new item</h2>
+                        <div className="col-12 col-md-4 mb-3 mb-md-0">
+                            <div className="loginFormInputGroup">
+                                <FormInput type={"text"}
+                                           id={"inputAddItemName"}
+                                           label={"Name"}
+                                           invalidFeedback={"You must name your item"}
+                                           forceCase={"title"}
+                                           value={addItemData.name}
+                                           onChange={(e, x) => {
+                                               setAddItemData(prevState => {
+                                                   return {...prevState, name: x}
+                                               })
+                                           }}/>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-4 mb-3  mb-md-0">
+                            <div className="loginFormInputGroup">
+                                <FormInput type={"text"}
+                                           id={"inputAddItemUnit"}
+                                           label={"Base unit"}
+                                           invalidFeedback={"You must specify a unit type"}
+                                           forceCase={"lower"}
+                                           value={addItemData.unit}
+                                           onChange={(e, x) => {
+                                               setAddItemData(prevState => {
+                                                   return {...prevState, unit: x}
+                                               })
+                                           }}/>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-2 mb-3  mb-md-0">
+                            <div className="loginFormInputGroup">
+                                <FormInput type={"number"}
+                                           id={"inputAddItemWarningLevel"}
+                                           label={"Warning level"}
+                                           invalidFeedback={"You must specify a warning level"}
+                                           min={0}
+                                           value={addItemData.warningLevel}
+                                           onChange={(e, x) => {
+                                               setAddItemData(prevState => {
+                                                   return {...prevState, warningLevel: x}
+                                               })
+                                           }}/>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-2 d-flex align-items-center">
+                            <button type={"submit"} className={"btn btn-success"}>Add</button>
+                        </div>
+                    </div>
+                </form>
                 <div className="row my-3">
-                    <h2>Add new item</h2>
-                    <div className="col-12 col-md-4 mb-3 mb-md-0">
-                        <div className="loginFormInputGroup">
-                            <FormInput type={"text"}
-                                       id={"inputAddItemName"}
-                                       label={"Name"}
-                                       invalidFeedback={"You must name your item"}
-                                       forceCase={"title"}
-                                       value={addData.name}
-                                       onChange={(e, x) => {
-                                           setAddData({...addData, name: x})
-                                       }}/>
-                        </div>
-                    </div>
-                    <div className="col-12 col-md-4 mb-3  mb-md-0">
-                        <div className="loginFormInputGroup">
-                            <FormInput type={"text"}
-                                       id={"inputAddItemUnit"}
-                                       label={"Base unit"}
-                                       invalidFeedback={"You must specify a unit type"}
-                                       forceCase={"lower"}
-                                       value={addData.unit}
-                                       onChange={(e, x) => {
-                                           setAddData({...addData, unit: x})
-                                       }}/>
-                        </div>
-                    </div>
-                    <div className="col-12 col-md-2 mb-3  mb-md-0">
-                        <div className="loginFormInputGroup">
-                            <FormInput type={"number"}
-                                       id={"inputAddItemWarningLevel"}
-                                       label={"Warning level"}
-                                       invalidFeedback={"You must specify a warning level"}
-                                       min={0}
-                                       value={addData.warningLevel}
-                                       onChange={(e, x) => {
-                                           setAddData({...addData, warningLevel: x})
-                                       }}/>
-                        </div>
-                    </div>
-                    <div className="col-12 col-md-2 d-flex align-items-center">
-                        <button type={"submit"} className={"btn btn-success"}>Add</button>
-                    </div>
+                    <h2>All items</h2>
+                    <Table
+                        headers={["ID", "Name", "Current stock", {text: "Warning level", colspan: 3}]}
+                        rows={makeItemRows(itemList)}
+                    />
                 </div>
-            </form>
-            <div className="row my-3">
-                <h2>All items</h2>
-                <Table
-                    headers={["ID", "Name", "Current stock", {text: "Warning level", colspan: 3}]}
-                    rows={itemList}
+                <ConfirmModal
+                    {...modalOptions}
+                    handleNo={() => {
+                        setModalOptions(prevState => {
+                            return {...prevState, show: false}
+                        });
+                    }}
+                    handleYes={() => deleteItem(modalOptions.deleteId)}
                 />
             </div>
-            <ConfirmModal
-                {...modalOptions}
-                handleNo={() => {
-                    setModalOptions(prevState => {
-                        return {...prevState, show: false}
-                    });
-                }}
-                handleYes={() => deleteItem(modalOptions.deleteId)}
-            />
-        </div>
-    );
-};
+        );
+    }
+;
 
 export default Items;
