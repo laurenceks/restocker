f68749688db3a3d15158132a9b47266210d97b4f
Laurence Summers <lks@live.co.uk>
2021-12-07 10:21:25 +0000

    Get locations now also fetchs stock at each location

--- public/php/locations/getAllLocations.php
+++ public/php/locations/getAllLocations.php
@@ -5,11 +5,26 @@ require "../common/db.php";
 
 $output = array("items" => array());
 $getAllLocations = $db->prepare("
-        SELECT * FROM locations
-        WHERE organisationId = :organisationId
-        AND deleted = 0
-        ");
-$getAllLocations->bindValue(':organisationId', $_SESSION["user"]->organisationId);
+SELECT 
+  *, 
+  (
+    SELECT 
+      CAST(SUM(transactions.quantity) AS INTEGER)
+    FROM 
+      transactions 
+    WHERE 
+      organisationId = :organisationId1 
+      AND deleted = 0 
+      AND locationId = locations.id
+  ) AS currentStock
+FROM 
+  locations 
+WHERE 
+  locations.organisationId = :organisationId2 
+  AND locations.deleted = 0;
+");
+$getAllLocations->bindValue(':organisationId1', $_SESSION["user"]->organisationId);
+$getAllLocations->bindValue(':organisationId2', $_SESSION["user"]->organisationId);
 $getAllLocations->execute();
 $output["locations"] = $getAllLocations->fetchAll(PDO::FETCH_ASSOC);
 
