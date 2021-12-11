9e6ba3d2c19d63c32ad9a828b30c91e4575d5b6b
Laurence Summers <lks@live.co.uk>
2021-12-11 10:39:49 +0000

    Made toasts stackable

--- src/components/Bootstrap/CompleteToast.js
+++ src/components/Bootstrap/CompleteToast.js
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types';
 import {Toast, ToastContainer} from "react-bootstrap";
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import {IoCheckmarkCircleOutline} from "react-icons/all";
+import {GlobalAppContext} from "../../App";
+import {log10} from "chart.js/helpers";
 
 const dividers = {
     s: 1000,
@@ -13,51 +15,74 @@ const dividers = {
     y: 3.154e+10
 }
 
-function CompleteToast({show, title, timestamp, bodyText, headerClass, showStateChange}) {
+const calculateTimeDiff = (timeInMs) => {
+    const diff = Date.now() - timeInMs;
+    switch (true) {
+        case (diff < dividers.s):
+            return "Just now";
+        case (diff < dividers.m):
+            return Math.floor(diff / dividers.s) + "s";
+        case (diff < dividers.h):
+            return Math.floor(diff / dividers.m) + "m";
+        case (diff < dividers.d):
+            return Math.floor(diff / dividers.h) + "h";
+        case (diff < dividers.w):
+            return Math.floor(diff / dividers.d) + "d";
+        case (diff < dividers.mo):
+            return Math.floor(diff / dividers.w) + "w";
+        case (diff < dividers.y):
+            return Math.floor(diff / dividers.mo) + "mo";
+        case (diff >= dividers.y):
+            return Math.floor(diff / dividers.y) + "y";
+        default:
+            return diff + "ms";
+    }
+}
+
+const timeUntilNext = (from, unit = "s") => {
+    let divider = dividers[unit];
+    return (Math.ceil(from / divider) * divider) - from;
+}
+
+function CompleteToast({show, title, timestamp, bodyText, headerClass, updateDeleteIds, id, deleteIds}) {
     const [showState, setShowState] = useState(show);
     const [timestampUpdated, setTimestampUpdated] = useState(null);
     const [timestampState, setTimestampState] = useState(timestamp);
     const [timestampText, setTimestampText] = useState("Just now");
+    const setToasts = useContext(GlobalAppContext)[0].setStateFunctions.toasts;
+    const shownOnce = useRef(false);
+    const deleteTimeout = useRef(null);
+    const hovering = useRef(false);
 
-    const calculateTimeDiff = (timeInMs) => {
-        const diff = Date.now() - timeInMs;
-        switch (true) {
-            case (diff < dividers.s):
-                return "Just now";
-            case (diff < dividers.m):
-                return Math.floor(diff / dividers.s) + "s";
-            case (diff < dividers.h):
-                return Math.floor(diff / dividers.m) + "m";
-            case (diff < dividers.d):
-                return Math.floor(diff / dividers.h) + "h";
-            case (diff < dividers.w):
-                return Math.floor(diff / dividers.d) + "d";
-            case (diff < dividers.mo):
-                return Math.floor(diff / dividers.w) + "w";
-            case (diff < dividers.y):
-                return Math.floor(diff / dividers.mo) + "mo";
-            case (diff >= dividers.y):
-                return Math.floor(diff / dividers.y) + "y";
-            default:
-                return diff + "ms";
+    const close = () => {
+        if (!hovering.current) {
+            setShowState(false);
         }
     }
-    const timeUntilNext = (from, unit = "s") => {
-        let divider = dividers[unit];
-        return (Math.ceil(from / divider) * divider) - from;
+
+    const setDeleteTimeout = () => {
+        if (shownOnce.current && !hovering.current) {
+            deleteTimeout.current = setTimeout(() => {
+                setToasts(prevState => prevState.filter(x => x.id !== id));
+            }, 2000)
+        }
     }
 
     useEffect(() => {
-        if (show) {
+        if (showState) {
             setTimestampUpdated(Date.now());
             setTimestampState(Date.now);
+        } else {
+            setDeleteTimeout();
         }
-        setShowState(show);
-    }, [show]);
+    }, [showState]);
 
     useEffect(() => {
-        showStateChange(showState)
-    }, [showState]);
+        if (!showState) {
+            setShowState(true);
+            shownOnce.current = true;
+        }
+    }, []);
 
     useEffect(() => {
         if (showState) {
@@ -72,24 +97,35 @@ function CompleteToast({show, title, timestamp, bodyText, headerClass, showState
     }, [timestampUpdated]);
 
     return (
-        <ToastContainer className="p-3" position="bottom-end">
-            <Toast style={{whiteSpace: "pre-wrap"}}
-                   onClose={() => setShowState(false)}
-                   onClick={() => setShowState(false)}
-                   show={showState}
-                   // delay={3000}
-                // autohide
-                   className="cursor-pointer">
-                <Toast.Header className={headerClass} closeButton={false}>
-                    <IoCheckmarkCircleOutline className={"smallIcon me-2"}/>
-                    <p className="fs-5 my-0 me-auto">{title}</p>
-                    <small>{timestampText}</small>
-                </Toast.Header>
-                <Toast.Body className={"position-relative"}>
-                    {bodyText}
-                </Toast.Body>
-            </Toast>
-        </ToastContainer>
+        <Toast style={{whiteSpace: "pre-wrap"}}
+               onClose={close}
+               onClick={close}
+               onMouseEnter={() => {
+                   hovering.current = true;
+                   if (deleteTimeout.current) {
+                       clearTimeout(deleteTimeout.current);
+                       deleteTimeout.current = null;
+                       setShowState(true);
+                   }
+               }}
+               onMouseLeave={() => {
+                   hovering.current = false;
+                   setTimeout(close, 3000);
+               }}
+               show={showState}
+               delay={4000}
+               autohide
+               id={id}
+               className="cursor-pointer">
+            <Toast.Header className={headerClass} closeButton={false}>
+                <IoCheckmarkCircleOutline className={"smallIcon me-2"}/>
+                <p className="fs-5 my-0 me-auto">{title}</p>
+                <small>{timestampText}</small>
+            </Toast.Header>
+            <Toast.Body className={"position-relative"}>
+                {bodyText}
+            </Toast.Body>
+        </Toast>
     );
 }
 
---
+++ src/components/Bootstrap/ToastStack.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import {ToastContainer} from "react-bootstrap";
+import CompleteToast from "./CompleteToast";
+import {useContext, useEffect, useRef, useState} from "react";
+import {GlobalAppContext} from "../../App";
+
+function ToastStack({toasts, position}) {
+    useEffect(() => {
+        console.log(toasts);
+    }, [toasts]);
+
+    return (
+        <ToastContainer className="p-3" position={position}>
+            {toasts.map((x, i) => {
+                return <CompleteToast key={"toast-" + x.id} {...x}/>
+            })}
+        </ToastContainer>
+    );
+}
+
+ToastStack.propTypes = {
+    position: PropTypes.string,
+    toasts: PropTypes.array
+};
+
+ToastStack.defaultProps = {
+    toasts: [],
+    position: "bottom-end"
+}
+
+export default ToastStack;
