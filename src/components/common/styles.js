aa1250254d2ebd9fb0c32c4d2522d941158190cf
Laurence Summers <lks@live.co.uk>
2021-12-14 16:23:25 +0000

    Added variantPairings lookup object

--- src/components/common/styles.js
+++ src/components/common/styles.js
@@ -1,9 +1,29 @@
+import {IoCheckmarkCircleOutline, IoCloseCircleOutline, IoWarningOutline} from "react-icons/all";
+
 const computedStyle = getComputedStyle(document.documentElement, null);
 const bootstrapColourNames = ["--bs-blue", "--bs-indigo", "--bs-purple", "--bs-pink", "--bs-red", "--bs-orange", "--bs-yellow", "--bs-green", "--bs-teal", "--bs-cyan", "--bs-white", "--bs-gray", "--bs-gray-dark", "--bs-primary", "--bs-secondary", "--bs-success", "--bs-info", "--bs-warning", "--bs-danger", "--bs-light", "--bs-dark", "--bs-font-sans-serif", "--bs-font-monospace", "--bs-gradient"]
 const bootstrapVariables = {};
 bootstrapColourNames.forEach((x) => {
     bootstrapVariables[x.replace("--bs-", "")] = computedStyle.getPropertyValue(x);
 });
+
+const variantPairings = {
+    success: {
+        icon: IoCheckmarkCircleOutline,
+        text: "text-white",
+        bg: "bg-success",
+    },
+    warning: {
+        icon: IoWarningOutline,
+        text: "text-black",
+        bg: "bg-warning",
+    },
+    danger: {
+        icon: IoCloseCircleOutline,
+        text: "text-white",
+        bg: "bg-danger",
+    }
+}
 
 const commonChartOptions = {
     responsive: true,
@@ -38,6 +58,4 @@ const commonChartOptions = {
     }
 }
 
-console.log(bootstrapVariables["font-sans-serif"])
-
-export {bootstrapVariables, commonChartOptions};
+export {bootstrapVariables, commonChartOptions, variantPairings};
