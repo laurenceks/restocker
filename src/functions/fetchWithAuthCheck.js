57f08a9e39ebed43aa78bab1ba935a86eb7e2fa3
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2021-12-29 16:29:16 +0000

    Made hash router default logout path, not just local

--- src/functions/fetchWithAuthCheck.js
+++ src/functions/fetchWithAuthCheck.js
@@ -1,6 +1,7 @@
 const fetchWithAuthCheck = (url, options, responseType = "json") => {
     const logoutDueToResponse = () => {
-        window.location = `${(window.location.hostname.startsWith("localhost") || window.location.hostname.startsWith("127") || window.location.hostname.startsWith("192")) && "/#"}/logout`;
+        // window.location = `${(window.location.hostname.startsWith("localhost") || window.location.hostname.startsWith("127") || window.location.hostname.startsWith("192")) && "/#"}/logout`;
+        window.location.replace("#/logout");
     }
     return fetch(url, options).then(async response => {
         if (response.status === 401) {
