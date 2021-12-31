0328b043c839cb075a13863ad70edbbc15b8bce6
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2021-12-31 19:30:00 +0000

    Renamed data prop to tableProps for clarity

--- src/components/common/tables/TableSection.js
+++ src/components/common/tables/TableSection.js
@@ -2,23 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Table from "./Table";
 
-const TableSection = ({title, data}) => {
+const TableSection = ({title, tableProps}) => {
     return (
         <div className="my-3">
             <h3>{title}</h3>
-            <Table title={"Active users"} headers={data.headers} rows={data.rows}/>
+            <Table {...tableProps}/>
         </div>
     );
 };
 
 TableSection.propTypes = {
     title: PropTypes.string,
-    data: PropTypes.object
+    tableProps: PropTypes.object
 };
 
 TableSection.defaultProps = {
     title: "Table",
-    data: null
+    tableProps: null
 };
 
 export default TableSection;
