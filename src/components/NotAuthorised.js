eb76ddd9cef173665e3d22d44b1b79bc23010a8f
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2021-12-31 11:55:25 +0000

    Fixed routing glitch where noMatch path always resolved on first load

--- src/components/NotAuthorised.js
+++ src/components/NotAuthorised.js
@@ -8,35 +8,20 @@ import ReVerify from "./login/ReVerify";
 const NotAuthorised = () => {
 
     return (
-        <Switch>
-            <div className="loginForm align-middle">
-                <img src="./img/logo.svg" alt="Restocker logo" className="w-100 p-3 mb-4"/>
-                <Route path="/verify">
-                    <Verify/>
-                </Route>
-                <Route path="/reVerify">
-                    <ReVerify/>
-                </Route>
-                <Route path="/login">
-                    <Login/>
-                </Route>
-                <Route path="/register">
-                    <Register/>
-                </Route>
-                <Route path="/registerOrganisation">
-                    <p>Register organisation</p>
-                </Route>
-                <Route path="/forgotPassword">
-                    <ForgotPassword/>
-                </Route>
-                <Route path="/resetPassword">
-                    <Verify type={"password"}/>
-                </Route>
+        <div className="loginForm align-middle">
+            <img src="./img/logo.svg" alt="Restocker logo" className="w-100 p-3 mb-4"/>
+            <Switch>
+                <Route path="/verify" component={Verify}/>
+                <Route path="/reVerify" component={ReVerify}/>
+                <Route path="/register" component={Register}/>
+                <Route path="/forgotPassword" component={ForgotPassword}/>
+                <Route path="/resetPassword" component={() => Verify("password")}/>
+                <Route path="/login" component={Login}/>
                 <Route>
                     <Redirect to="/login"/>
                 </Route>
-            </div>
-        </Switch>
+            </Switch>
+        </div>
     );
 };
 
