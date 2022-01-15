73837daa25a9f6b1fff62c4af11e30e9d885f9ce
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-01-15 17:15:21 +0000

    Removed logging

--- src/hooks/useInitialise.js
+++ src/hooks/useInitialise.js
@@ -3,7 +3,6 @@ import {useRef} from "react";
 export const useInitialise = (callback) => {
     const runOnce = useRef(false);
     if (!runOnce.current) {
-        console.log("Running!!!")
         callback();
         runOnce.current = true;
     }
