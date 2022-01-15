4bb9def0df99a753bdaebeccd52c1f60c7f40558
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-01-15 12:24:41 +0000

    Added extra forced server side logout if user security checks fail

--- public/php/security/endProcessDueToInvalidLogin.php
+++ public/php/security/endProcessDueToInvalidLogin.php
@@ -3,7 +3,7 @@ function endProcessDueToInvalidLogin($msg)
 {
     //user not logged in or login status invalid - proceed no further
     echo json_encode(array("failedLoginCheck" => true, "errorMessage" => $msg));
-    //TODO force logout?
+    require "../login/logout.php"; //TODO check is this going to a cause an error by running logout twice?
     die();
 }
 
