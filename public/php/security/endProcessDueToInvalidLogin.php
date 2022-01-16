81e5eac842333ae41e3c3d8676c28609e63638e8
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-01-16 22:24:43 +0000

    Removed TODO

--- public/php/security/endProcessDueToInvalidLogin.php
+++ public/php/security/endProcessDueToInvalidLogin.php
@@ -2,8 +2,8 @@
 function endProcessDueToInvalidLogin($msg)
 {
     //user not logged in or login status invalid - proceed no further
+    require "../login/logout.php";
     echo json_encode(array("failedLoginCheck" => true, "errorMessage" => $msg));
-    require "../login/logout.php"; //TODO check is this going to a cause an error by running logout twice?
     die();
 }
 
