818b78376079106ae0412f69d6250acde2aad816
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-01-17 12:49:59 +0000

    Split account page into form components

--- src/components/profile/Account.js
+++ src/components/profile/Account.js
@@ -1,205 +1,24 @@
-import FormInput from "../common/forms/FormInput";
 import {useContext, useRef, useState} from "react"
-import validateForm from "../../functions/formValidation";
 import {GlobalAppContext} from "../../App";
-import useFetch from "../../hooks/useFetch";
-import {useNavigate} from "react-router-dom";
-import {variantPairings} from "../common/styles";
 import EditAccountForm from "./accountForms/EditAccountForm";
+import EditAccountEmailForm from "./accountForms/EditAccountEmailForm";
+import EditAccountPasswordForm from "./accountForms/EditAccountPasswordForm";
+import DeleteAccountForm from "./accountForms/DeleteAccountForm";
 
 const Account = () => {
 
-    const [globalAppContext, setGlobalAppContext] = useContext(GlobalAppContext);
-
-    const fetchHook = useFetch();
-    const history = useNavigate();
-
-    const [user, setUser] = useState({
-        email: globalAppContext.user.email,
-        firstName: globalAppContext.user.firstName,
-        lastName: globalAppContext.user.lastName
-    });
-
-    const emailFormRef = useRef();
-    const passwordFormRef = useRef();
-    const deleteFormRef = useRef();
-    const passwordFormReset= useRef();
-
-    const editEmail = (form) => {
-        fetchHook({
-            type: "editAccountEmail",
-            options: {
-                method: "POST",
-                body: JSON.stringify({
-                    ...form.values,
-                    firstName: globalAppContext.user.firstName,
-                    lastName: globalAppContext.user.lastName
-                }),
-            },
-            callback: (response) => {
-                setGlobalAppContext(prevState => {
-                    return {
-                        ...prevState,
-                        user: {
-                            ...prevState.user,
-                            email: form.values.inputAccountEmail,
-                        }
-                    }
-                });
-                emailFormRef.current.reset()
-            }
-        })
-    }
-    const editPassword = (form) => {
-        fetchHook({
-            type: "editAccountPassword",
-            options: {
-                method: "POST",
-                body: JSON.stringify({
-                    ...form.values
-                }),
-            },
-            callback: () => passwordFormRef.current.reset()
-        })
-    }
-    const deleteAccount = (e) => {
-        console.log(e);
-    }
+    const [globalAppContext] = useContext(GlobalAppContext);
 
     return (
         <div className="container">
             <div className={"row align-items-center mb-3"}>
-                <h1>{globalAppContext.user.firstName}'s profile</h1>
+                <h1>{globalAppContext.user.firstName}'s account</h1>
                 <p className="m-0">{globalAppContext.user.superAdmin ? "Super admin" : globalAppContext.user.admin ? "Admin" : "User"}</p>
             </div>
             <EditAccountForm/>
-            <form ref={emailFormRef} onSubmit={(e) => {
-                validateForm(e, emailFormRef, (form) => {
-                    globalAppContext.setStateFunctions.confirmModal(prevState => {
-                        return {
-                            ...prevState,
-                            show: true,
-                            headerClass: variantPairings.warning.header,
-                            yesButtonVariant: "warning",
-                            bodyText: `Are you sure you want to change your email to ${form.values.inputAccountEmail}?`,
-                            handleYes: () => editEmail(form)
-                        }
-                    })
-                })
-            }}>
-                <div className="row mb-3 align-items-center">
-                    <div className="col-12 col-md-3 mb-3 mb-md-0 formInputGroup">
-                        <FormInput type={"text"}
-                                   id={"inputAccountEmail"}
-                                   label={"Email address"}
-                                   onChange={(id, val) => {
-                                       setUser({
-                                           ...user,
-                                           email: val
-                                       });
-                                   }}
-                                   value={user.email}
-                        />
-                    </div>
-                    <div className="col-12 col-md-3 mb-3 mb-md-0 formInputGroup">
-                        <FormInput type={"password"}
-                                   id={"inputAccountPassword1"}
-                                   label={"Password"}
-                                   invalidFeedback={"Please enter your password"}
-                                   passwordId={1}/>
-                    </div>
-                    <div className="col-12 col-md-3 mb-3 mb-md-0 formInputGroup">
-                        <FormInput type={"password"}
-                                   id={"inputAccountPassword2"}
-                                   label={"Confirm password"}
-                                   invalidFeedback={"Passwords do not match"}
-                                   passwordId={1}/>
-                    </div>
-                    <div className="col-12 col-md-3">
-                        <button type="submit" className="btn btn-primary w-100">Change email</button>
-                    </div>
-                </div>
-            </form>
-            <form ref={passwordFormRef}
-                  onSubmit={(e) => {
-                      validateForm(e, passwordFormRef, (form) => {
-                          globalAppContext.setStateFunctions.confirmModal(prevState => {
-                              return {
-                                  ...prevState,
-                                  show: true,
-                                  headerClass: variantPairings.warning.header,
-                                  yesButtonVariant: "warning",
-                                  bodyText: `Are you sure you want to change your password?`,
-                                  handleYes: () => editPassword(form)
-                              }
-                          })
-                      })
-                  }}
-                  onReset={() =>{
-                      passwordFormReset.current = Date.now();
-                  }}
-            >
-                <div className="row align-items-center mb-3">
-                    <div className="col-12 col-md-3 mb-3 mb-md-0 formInputGroup">
-                        <FormInput type={"password"}
-                                   id={"inputAccountOldPassword"}
-                                   label={"Old password"}
-                                   invalidFeedback={"Please enter your current password"}
-                                   reset={passwordFormReset.current}
-                        />
-                    </div>
-                    <div className="col-12 col-md-3 mb-3 mb-md-0 formInputGroup">
-                        <FormInput type={"password"}
-                                   id={"inputAccountNewPassword1"}
-                                   label={"New password"}
-                                   invalidFeedback={"Please enter a password at least eight characters long with one lower case letter, one capital, one number and a symbol"}
-                                   passwordId={2}
-                                   reset={passwordFormReset.current}
-                        />
-                    </div>
-                    <div className="col-12 col-md-3 mb-3 mb-md-0 formInputGroup">
-                        <FormInput type={"password"}
-                                   id={"inputAccountNewPassword2"}
-                                   label={"Confirm new password"}
-                                   invalidFeedback={"Passwords do not match"}
-                                   passwordId={2}
-                                   reset={passwordFormReset.current}
-                        />
-                    </div>
-                    <div className="col-12 col-md-3">
-                        <button type="submit" className="btn btn-primary w-100">Change password</button>
-                    </div>
-                </div>
-            </form>
-            <form ref={deleteFormRef}
-                  onSubmit={(e) => {
-                      validateForm(e, deleteFormRef, deleteAccount)
-                  }}
-            >
-                <div className="row align-items-center mb-3">
-                    <div className="col-12 col-md-3 mb-3 mb-md-0 formInputGroup">
-                    </div>
-                    <div className="col-12 col-md-3 mb-3 mb-md-0 formInputGroup">
-                        <FormInput type={"password"}
-                                   id={"inputAccountDeletePassword1"}
-                                   label={"Current password"}
-                                   invalidFeedback={"Please enter your current password"}
-                                   passwordId={3}
-                        />
-                    </div>
-                    <div className="col-12 col-md-3 mb-3 mb-md-0 formInputGroup">
-                        <FormInput type={"password"}
-                                   id={"inputAccountDeletePassword2"}
-                                   label={"Confirm password"}
-                                   invalidFeedback={"Passwords do not match"}
-                                   passwordId={3}
-                        />
-                    </div>
-                    <div className="col-12 col-md-3">
-                        <button type="submit" className="btn btn-danger w-100">Delete account</button>
-                    </div>
-                </div>
-            </form>
+            <EditAccountEmailForm/>
+            <EditAccountPasswordForm/>
+            <DeleteAccountForm/>
         </div>
     );
 };
---
+++ src/components/profile/accountForms/DeleteAccountForm.js
@@ -0,0 +1,77 @@
+import FormInput from "../../common/forms/FormInput";
+import {useContext, useRef} from "react"
+import validateForm from "../../../functions/formValidation";
+import {GlobalAppContext} from "../../../App";
+import useFetch from "../../../hooks/useFetch";
+import {variantPairings} from "../../common/styles";
+
+const DeleteAccountForm = () => {
+
+    const [globalAppContext] = useContext(GlobalAppContext);
+
+    const fetchHook = useFetch();
+
+    const formRef = useRef();
+    const formReset = useRef();
+
+    const deleteAccount = (form) => {
+        console.log(form);
+    }
+
+    return (
+        <form ref={formRef}
+              onSubmit={(e) => {
+                  validateForm(e, formRef, (form) => {
+                      globalAppContext.setStateFunctions.confirmModal(prevState => {
+                          return {
+                              ...prevState,
+                              show: true,
+                              bodyText: "Are you sure you want to delete you account?\n\nYour account will be deleted. A masked version of your email address (EX) will be kept to track previous changes. Your full email address will be deleted.\n\nTo regain access you will need to register again from scratch.\n\n",
+                              handleYes: () => {
+                                  const maskedEmail = globalAppContext.user.email.match(/(\w)(\w*?)(\w?)@(\w)(\w*)([\.\w]+)/)
+                                  globalAppContext.setStateFunctions.confirmModal(prevState => {
+                                      return {
+                                          ...prevState,
+                                          show: true,
+                                          bodyText: "Are you really sure? This action CANNOT be undone!",
+                                          handleYes: () => deleteAccount(form)
+                                      }
+                                  })
+                              }
+                          }
+                      })
+                  })
+              }}
+              onReset={() => {
+                  formReset.current = Date.now();
+              }}
+        >
+            <div className="row align-items-center mb-3">
+                <div className="col-12 col-md-3 mb-3 mb-md-0 formInputGroup">
+                </div>
+                <div className="col-12 col-md-3 mb-3 mb-md-0 formInputGroup">
+                    <FormInput type={"password"}
+                               id={"inputAccountDeletePassword1"}
+                               label={"Current password"}
+                               invalidFeedback={"Please enter your current password"}
+                               passwordId={3}
+                               reset={formReset.current}
+                    />
+                </div>
+                <div className="col-12 col-md-3 mb-3 mb-md-0 formInputGroup">
+                    <FormInput type={"password"}
+                               id={"inputAccountDeletePassword2"}
+                               label={"Confirm password"}
+                               invalidFeedback={"Passwords do not match"}
+                               passwordId={3}
+                               reset={formReset.current}
+                    />
+                </div>
+                <div className="col-12 col-md-3">
+                    <button type="submit" className="btn btn-danger w-100">Delete account</button>
+                </div>
+            </div>
+        </form>)
+};
+
+export default DeleteAccountForm;
