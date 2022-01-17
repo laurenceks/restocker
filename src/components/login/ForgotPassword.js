687ab685d666ddec795e51daee120b87fa088fa5
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-01-17 18:14:34 +0000

    Made reverify link a button

--- src/components/login/ForgotPassword.js
+++ src/components/login/ForgotPassword.js
@@ -3,10 +3,12 @@ import FormInput from "../common/forms/FormInput";
 import FormLink from "../common/forms/FormLink";
 import validateForm from "../../functions/formValidation.js"
 import LoginFeedback from "./LoginFeedback";
+import {useNavigate} from "react-router-dom";
 
 const Forgot = () => {
     const [forgotFeedback, setForgotFeedback] = useState({inProgress: false});
     const forgotForm = useRef();
+    const history = useNavigate();
     const forgot = formOutput => {
         setForgotFeedback({...forgotFeedback, inProgress: true})
         fetch("./php/login/forgot/forgot.php", {
@@ -43,7 +45,8 @@ const Forgot = () => {
                 {(forgotFeedback?.feedback && !forgotFeedback?.inProgress) &&
                 <LoginFeedback feedbackText={forgotFeedback.feedback} feedbackClass={forgotFeedback.feedbackClass}/>}
                 {forgotFeedback?.feedback === "Email not verified" &&
-                <FormLink to={"/reVerify"} label={"Re-send verification email"}/>}
+                <button className="w-100 btn btn-lg btn-primary mt-3" onClick={() => history("/reVerify")}>Resend
+                    verification email</button>}
                 <FormLink to={"/login"} label={"Back to login"}/>
                 <p className="my-3 text-muted">&copy; Laurence Summers 2021</p></fieldset>
         </form>
